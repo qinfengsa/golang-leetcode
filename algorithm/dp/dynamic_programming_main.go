c47ea8d73f0c5e1515b2ac8b699eb603d8774b0f
qinfengsa <15800012123@163.com>
2021-07-13 12:57:07 +0800

    LeetCode 2021-07-13 61. 旋转链表 62. 不同路径 63. 不同路径 II 64. 最小路径和 65. 有效数字

--- algorithm/dp/dynamic_programming_main.go
+++ algorithm/dp/dynamic_programming_main.go
@@ -400,5 +400,141 @@ func canJump(nums []int) bool {
 		}
 	}
 	return pos == 0
+}
 
+// 62. 不同路径
+// 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+//
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+// 问总共有多少条不同的路径？
+//
+// 示例 1：
+// 输入：m = 3, n = 7 输出：28
+//
+// 示例 2：
+// 输入：m = 3, n = 2 输出：3
+// 解释：
+// 从左上角开始，总共有 3 条路径可以到达右下角。
+// 1. 向右 -> 向下 -> 向下
+// 2. 向下 -> 向下 -> 向右
+// 3. 向下 -> 向右 -> 向下
+//
+// 示例 3：
+// 输入：m = 7, n = 3 输出：28
+//
+// 示例 4：
+// 输入：m = 3, n = 3 输出：6
+//
+// 提示：
+// 1 <= m, n <= 100
+// 题目数据保证答案小于等于 2 * 109
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = 1
+	for i := 1; i < m; i++ {
+		dp[i][0] = dp[i-1][0]
+	}
+	for j := 1; j < n; j++ {
+		dp[0][j] = dp[0][j-1]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+// 63. 不同路径 II
+// 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+// 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+//
+// 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+// 网格中的障碍物和空位置分别用 1 和 0 来表示。
+//
+// 示例 1：
+// 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]] 输出：2
+// 解释：
+// 3x3 网格的正中间有一个障碍物。
+// 从左上角到右下角一共有 2 条不同的路径：
+// 1. 向右 -> 向右 -> 向下 -> 向下
+// 2. 向下 -> 向下 -> 向右 -> 向右
+//
+// 示例 2：
+// 输入：obstacleGrid = [[0,1],[0,0]] 输出：1
+//
+// 提示：
+// m == obstacleGrid.length
+// n == obstacleGrid[i].length
+// 1 <= m, n <= 100
+// obstacleGrid[i][j] 为 0 或 1
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	if obstacleGrid[0][0] == 1 {
+		return 0
+	}
+	dp[0][0] = 1
+	for i := 1; i < m; i++ {
+		if obstacleGrid[i][0] == 1 {
+			break
+		}
+		dp[i][0] = dp[i-1][0]
+	}
+	for j := 1; j < n; j++ {
+		if obstacleGrid[0][j] == 1 {
+			break
+		}
+		dp[0][j] = dp[0][j-1]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if obstacleGrid[i][j] == 1 {
+				continue
+			}
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+// 64. 最小路径和
+// 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+// 说明：每次只能向下或者向右移动一步。
+//
+// 示例 1：
+// 输入：grid = [[1,3,1],[1,5,1],[4,2,1]] 输出：7
+// 解释：因为路径 1→3→1→1→1 的总和最小。
+//
+// 示例 2：
+// 输入：grid = [[1,2,3],[4,5,6]] 输出：12
+//
+// 提示：
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 200
+// 0 <= grid[i][j] <= 100
+func minPathSum(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+
+	for i := 1; i < m; i++ {
+
+		grid[i][0] += grid[i-1][0]
+	}
+	for j := 1; j < n; j++ {
+		grid[0][j] += grid[0][j-1]
+	}
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+		}
+	}
+
+	return grid[m-1][n-1]
 }
--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 // 13. 罗马数字转整数
@@ -1639,4 +1640,134 @@ func isMatch(s string, p string) bool {
 	}
 
 	return match[m][n]
+}
+
+// 65. 有效数字
+// 有效数字（按顺序）可以分成以下几个部分：
+//
+// 一个 小数 或者 整数
+// （可选）一个 'e' 或 'E' ，后面跟着一个 整数
+// 小数（按顺序）可以分成以下几个部分：
+//
+// （ 可选）一个符号字符（'+' 或 '-'）
+//  下述格式之一：
+//  至少一位数字，后面跟着一个点 '.'
+//  至少一位数字，后面跟着一个点 '.' ，后面再跟着至少一位数字
+//  一个点 '.' ，后面跟着至少一位数字
+//  整数（按顺序）可以分成以下几个部分：
+//
+// （可选）一个符号字符（'+' 或 '-'）
+//   至少一位数字
+//   部分有效数字列举如下：
+//
+//   ["2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"]
+//  部分无效数字列举如下：
+//
+//  ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"]
+//  给你一个字符串 s ，如果 s 是一个 有效数字 ，请返回 true 。
+//
+// 示例 1：
+// 输入：s = "0" 输出：true
+//
+// 示例 2：
+// 输入：s = "e" 输出：false
+//
+// 示例 3：
+// 输入：s = "." 输出：false
+//
+// 示例 4：
+// 输入：s = ".1" 输出：true
+//
+// 提示：
+// 1 <= s.length <= 20
+// s 仅含英文字母（大写和小写），数字（0-9），加号 '+' ，减号 '-' ，或者点 '.' 。
+func isNumber2(s string) bool {
+	// 初始状态 符号 整数 左无整数小数点 小数 指数e 指数符号 有效数字
+	const S0, S1, S2, S3, S4, S5, S6, S7 = 1, 2, 4, 8, 16, 32, 64, 128
+
+	state := 1
+	for _, c := range s {
+		switch state {
+		case S0:
+			{
+				if c == '+' || c == '-' {
+					state = S1
+				} else if unicode.IsDigit(c) {
+					state = S2
+				} else if c == '.' {
+					state = S3
+				} else {
+					return false
+				}
+			}
+		case S1:
+			{
+				if unicode.IsDigit(c) {
+					state = S2
+				} else if c == '.' {
+					state = S3
+				} else {
+					return false
+				}
+
+			}
+		case S2:
+			{
+				if c == '.' {
+					state = S4
+				} else if c == 'e' || c == 'E' {
+					state = S5
+				} else if !unicode.IsDigit(c) {
+					return false
+				}
+
+			}
+		case S3:
+			{
+				if unicode.IsDigit(c) {
+					state = S4
+				} else {
+					return false
+				}
+
+			}
+		case S4:
+			{
+				if c == 'e' || c == 'E' {
+					state = S5
+				} else if !unicode.IsDigit(c) {
+					return false
+				}
+
+			}
+		case S5:
+			{
+				if c == '+' || c == '-' {
+					state = S6
+				} else if unicode.IsDigit(c) {
+					state = S7
+				} else {
+					return false
+				}
+			}
+		case S6:
+			{
+				if unicode.IsDigit(c) {
+					state = S7
+				} else {
+					return false
+				}
+			}
+		case S7:
+			{
+				if !unicode.IsDigit(c) {
+					return false
+				}
+
+			}
+		}
+
+	}
+
+	return state == S2 || state == S4 || state == S7
 }
