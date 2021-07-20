402d2e885009023e198069b3353331de3ef9ead2
qinfengsa <15800012123@163.com>
2021-07-20 13:05:02 +0800

    LeetCode 2021-07-20 66. 加一 67. 二进制求和 68. 文本左右对齐 69. x 的平方根 70. 爬楼梯

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -1771,3 +1771,113 @@ func isNumber2(s string) bool {
 
 	return state == S2 || state == S4 || state == S7
 }
+
+// 68. 文本左右对齐
+// 给定一个单词数组和一个长度 maxWidth，重新排版单词，使其成为每行恰好有 maxWidth 个字符，且左右两端对齐的文本。
+// 你应该使用“贪心算法”来放置给定的单词；也就是说，尽可能多地往每行中放置单词。必要时可用空格 ' ' 填充，使得每行恰好有 maxWidth 个字符。
+// 要求尽可能均匀分配单词间的空格数量。如果某一行单词间的空格不能均匀分配，则左侧放置的空格数要多于右侧的空格数。
+// 文本的最后一行应为左对齐，且单词之间不插入额外的空格。
+//
+// 说明:
+// 单词是指由非空格字符组成的字符序列。
+// 每个单词的长度大于 0，小于等于 maxWidth。
+// 输入单词数组 words 至少包含一个单词。
+//
+// 示例 1:
+// 输入: words = ["This", "is", "an", "example", "of", "text", "justification."] maxWidth = 16
+// 输出:
+// [
+//   "This    is    an",
+//   "example  of text",
+//   "justification.  "
+// ]
+// 示例 2:
+// 输入: words = ["What","must","be","acknowledgment","shall","be"] maxWidth = 16
+// 输出:
+// [
+//  "What   must   be",
+//  "acknowledgment  ",
+//  "shall be        "
+// ]
+// 解释: 注意最后一行的格式应为 "shall be    " 而不是 "shall     be",
+//     因为最后一行应为左对齐，而不是左右两端对齐。
+//     第二行同样为左对齐，这是因为这行只包含一个单词。
+//
+// 示例 3:
+// 输入: words = ["Science","is","what","we","understand","well","enough","to","explain",
+//         "to","a","computer.","Art","is","everything","else","we","do"] maxWidth = 20
+// 输出:
+// [
+//  "Science  is  what we",
+//  "understand      well",
+//  "enough to explain to",
+//  "a  computer.  Art is",
+//  "everything  else  we",
+//  "do                  "
+// ]
+func fullJustify(words []string, maxWidth int) []string {
+	result := make([]string, 0)
+	size, cnt := 0, 0
+
+	getRowWord := func(cnt, end, blankCnt int) string {
+		var builder strings.Builder
+		avgBlankCnt, leftBlankCnt := blankCnt, 0
+
+		if cnt > 1 {
+			avgBlankCnt = blankCnt / (cnt - 1)
+			leftBlankCnt = blankCnt % (cnt - 1)
+		}
+		var blankBuilder strings.Builder
+		for i := 0; i < avgBlankCnt; i++ {
+			blankBuilder.WriteByte(' ')
+		}
+
+		for i := end - cnt + 1; i <= end; i++ {
+			builder.WriteString(words[i])
+			if i < end {
+				builder.WriteString(blankBuilder.String())
+				if leftBlankCnt > 0 {
+					builder.WriteByte(' ')
+					leftBlankCnt--
+				}
+			}
+		}
+		if cnt == 1 {
+			builder.WriteString(blankBuilder.String())
+		}
+		return builder.String()
+	}
+	idx := 0
+	for idx < len(words) {
+		word := words[idx]
+		if size+len(word) <= maxWidth-cnt {
+			size += len(word)
+			cnt++
+			idx++
+		} else {
+			rowWord := getRowWord(cnt, idx-1, maxWidth-size)
+			result = append(result, rowWord)
+			size = 0
+			cnt = 0
+		}
+	}
+	var lastRow strings.Builder
+	// 处理最后一行
+	if size > 0 {
+		idx = len(words) - 1
+
+		for i := idx - cnt + 1; i <= idx; i++ {
+			lastRow.WriteString(words[i])
+			if i < idx {
+				// 最后一行一个空格
+				lastRow.WriteByte(' ')
+			}
+		}
+	}
+	leftCnt := maxWidth - lastRow.Len()
+	for i := 0; i < leftCnt; i++ {
+		lastRow.WriteByte(' ')
+	}
+	result = append(result, lastRow.String())
+	return result
+}
