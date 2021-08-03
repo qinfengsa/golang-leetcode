b0ccf62dd9ae5ccfb044ae2c3f587b2cbd0bee6d
qinfengsa <15800012123@163.com>
2021-08-03 12:51:55 +0800

    LeetCode 2021-08-03 101. 对称二叉树 102. 二叉树的层序遍历 103. 二叉树的锯齿形层序遍历 104. 二叉树的最大深度 105. 从前序与中序遍历序列构造二叉树

--- data-structure/tree/tree_main.go
+++ data-structure/tree/tree_main.go
@@ -51,80 +51,6 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	return false
 }
 
-// 101. 对称二叉树
-// 给定一个二叉树，检查它是否是镜像对称的。
-// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
-//
-//     1
-//    / \
-//   2   2
-//  / \ / \
-// 3  4 4  3
-//
-// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
-//
-//    1
-//   / \
-//  2   2
-//   \   \
-//   3    3
-//
-//
-// 进阶：
-//
-// 你可以运用递归和迭代两种方法解决这个问题吗？
-func isSymmetric(root *TreeNode) bool {
-	if root == nil {
-		return true
-	}
-	return isSymmetric2(root.Left, root.Right)
-}
-
-func isSymmetric2(left *TreeNode, right *TreeNode) bool {
-	if left == nil && right == nil {
-		return true
-	}
-	if left != nil && right != nil {
-		if left.Val != right.Val {
-			return false
-		}
-		return isSymmetric2(left.Left, right.Right) && isSymmetric2(left.Right, right.Left)
-	}
-	return false
-}
-
-// 104. 二叉树的最大深度
-// 给定一个二叉树，找出其最大深度。
-//
-// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
-//
-// 说明: 叶子节点是指没有子节点的节点。
-//
-// 示例：
-// 给定二叉树 [3,9,20,null,null,15,7]，
-//    3
-//   / \
-//  9  20
-//    /  \
-//   15   7
-// 返回它的最大深度 3 。
-func maxDepth(root *TreeNode) int {
-	maxVal = 0
-	getDepth(root, 1)
-	return maxVal
-}
-
-var maxVal int = 0
-
-func getDepth(root *TreeNode, depth int) {
-	if root == nil {
-		depth = max(maxVal, depth)
-		return
-	}
-	getDepth(root.Left, depth+1)
-	getDepth(root.Right, depth+1)
-}
-
 // 107. 二叉树的层次遍历 II
 // 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
 //
@@ -1302,4 +1228,241 @@ func recoverTree(root *TreeNode) {
 	inOrder(root)
 	node1.Val, node2.Val = node2.Val, node1.Val
 
+}
+
+// 101. 对称二叉树
+// 给定一个二叉树，检查它是否是镜像对称的。
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+//
+//     1
+//    / \
+//   2   2
+//  / \ / \
+// 3  4 4  3
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+//
+//    1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+// 进阶：
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	var symmetric func(left *TreeNode, right *TreeNode) bool
+
+	symmetric = func(left *TreeNode, right *TreeNode) bool {
+		if left == nil && right == nil {
+			return true
+		}
+		if left != nil && right != nil {
+			if left.Val != right.Val {
+				return false
+			}
+			return symmetric(left.Left, right.Right) && symmetric(left.Right, right.Left)
+		}
+		return false
+	}
+
+	return symmetric(root.Left, root.Right)
+}
+
+// 102. 二叉树的层序遍历
+// 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+//
+// 示例：
+// 二叉树：[3,9,20,null,null,15,7],
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 返回其层序遍历结果：
+// [
+//  [3],
+//  [9,20],
+//  [15,7]
+// ]
+func levelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+	queue := list.New()
+	queue.PushBack(root)
+
+	for queue.Len() > 0 {
+		size := queue.Len()
+		level := make([]int, 0)
+		for i := 0; i < size; i++ {
+			front := queue.Front()
+			queue.Remove(front)
+			node := front.Value.(*TreeNode)
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		result = append(result, level)
+	}
+
+	return result
+}
+
+// 103. 二叉树的锯齿形层序遍历
+// 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+//
+// 例如：
+// 给定二叉树 [3,9,20,null,null,15,7],
+//
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 返回锯齿形层序遍历如下：
+//
+// [
+//  [3],
+//  [20,9],
+//  [15,7]
+// ]
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+
+	getNextList := func(treeList []*TreeNode, isLeft bool) []*TreeNode {
+		nextList := make([]*TreeNode, 0)
+		size := len(treeList)
+		for i := size - 1; i >= 0; i-- {
+			node := treeList[i]
+			if isLeft {
+				if node.Left != nil {
+					nextList = append(nextList, node.Left)
+				}
+				if node.Right != nil {
+					nextList = append(nextList, node.Right)
+				}
+			} else {
+				if node.Right != nil {
+					nextList = append(nextList, node.Right)
+				}
+				if node.Left != nil {
+					nextList = append(nextList, node.Left)
+				}
+			}
+		}
+
+		return nextList
+	}
+
+	treeList := []*TreeNode{root}
+	isLeft := true
+	for len(treeList) > 0 {
+		isLeft = !isLeft
+		row := make([]int, 0)
+		for i := range treeList {
+			row = append(row, treeList[i].Val)
+		}
+		treeList = getNextList(treeList, isLeft)
+		result = append(result, row)
+	}
+
+	return result
+}
+
+// 104. 二叉树的最大深度
+// 给定一个二叉树，找出其最大深度。
+//
+// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+//
+// 说明: 叶子节点是指没有子节点的节点。
+//
+// 示例：
+// 给定二叉树 [3,9,20,null,null,15,7]，
+//    3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 返回它的最大深度 3 。
+func maxDepth(root *TreeNode) int {
+	maxVal := 0
+
+	var getDepth func(root *TreeNode, depth int)
+
+	getDepth = func(root *TreeNode, depth int) {
+		if root == nil {
+			depth = max(maxVal, depth)
+			return
+		}
+		getDepth(root.Left, depth+1)
+		getDepth(root.Right, depth+1)
+	}
+
+	getDepth(root, 0)
+	return maxVal
+}
+
+// 105. 从前序与中序遍历序列构造二叉树
+//
+// 给定一棵树的前序遍历 preorder 与中序遍历  inorder。请构造二叉树并返回其根节点。
+//
+// 示例 1:
+// Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+// Output: [3,9,20,null,null,15,7]
+//
+// 示例 2:
+// Input: preorder = [-1], inorder = [-1] Output: [-1]
+//
+// 提示:
+// 1 <= preorder.length <= 3000
+// inorder.length == preorder.length
+// -3000 <= preorder[i], inorder[i] <= 3000
+// preorder 和 inorder 均无重复元素
+// inorder 均出现在 preorder
+// preorder 保证为二叉树的前序遍历序列
+// inorder 保证为二叉树的中序遍历序列
+func buildTree(preorder []int, inorder []int) *TreeNode {
+
+	idxMap := make(map[int]int)
+	for i := range inorder {
+		idxMap[inorder[i]] = i
+	}
+	var build func(preStart, preEnd, inStart, inEnd int) *TreeNode
+
+	build = func(preStart, preEnd, inStart, inEnd int) *TreeNode {
+		if preStart > preEnd || inStart > inEnd {
+			return nil
+		}
+		rootVal := preorder[preStart]
+		root := &TreeNode{
+			Val: rootVal,
+		}
+		inRootIdx := idxMap[rootVal]
+		leftLen, rightLen := inRootIdx-inStart, inEnd-inRootIdx
+		if leftLen > 0 {
+			root.Left = build(preStart+1, preStart+leftLen, inStart, inRootIdx-1)
+		}
+		if rightLen > 0 {
+			root.Right = build(preEnd-rightLen+1, preEnd, inRootIdx+1, inEnd)
+		}
+
+		return root
+	}
+	size := len(preorder)
+	return build(0, size-1, 0, size-1)
 }
