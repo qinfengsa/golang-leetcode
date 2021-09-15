9a125be4f859af37bc7d111b4a1b0ffc484ba903
qinfengsa <15800012123@163.com>
2021-09-15 11:56:56 +0800

    LeetCode 2021-09-15 126. 单词接龙 II 127. 单词接龙 128. 最长连续序列 129. 求根节点到叶节点数字之和 130. 被围绕的区域

--- data-structure/arrays/arrays_main.go
+++ data-structure/arrays/arrays_main.go
@@ -2399,3 +2399,115 @@ func maximalRectangle(matrix [][]byte) int {
 	}
 	return result
 }
+
+// 128. 最长连续序列
+// 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+//
+// 请你设计并实现时间复杂度为 O(n) 的算法解决此问题。
+//
+// 示例 1：
+// 输入：nums = [100,4,200,1,3,2] 输出：4
+// 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+//
+// 示例 2：
+// 输入：nums = [0,3,7,2,5,8,4,6,0,1] 输出：9
+//
+// 提示：
+// 0 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	result := 1
+	numMap := make(map[int]bool)
+	for _, num := range nums {
+		numMap[num] = true
+	}
+	for num := range numMap {
+		if numMap[num-1] {
+			continue
+		}
+		size := 1
+		for numMap[num+1] {
+			num++
+			size++
+		}
+		result = max(result, size)
+	}
+	return result
+}
+
+// 130. 被围绕的区域
+// 给你一个 m x n 的矩阵 board ，由若干字符 'X' 和 'O' ，找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+//
+// 示例 1：
+// 输入：board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+// 输出：[["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+// 解释：被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+//
+// 示例 2：
+// 输入：board = [["X"]] 输出：[["X"]]
+//
+// 提示：
+// m == board.length
+// n == board[i].length
+// 1 <= m, n <= 200
+// board[i][j] 为 'X' 或 'O'
+func solve(board [][]byte) {
+	m, n := len(board), len(board[0])
+
+	var update func(i, j int)
+	update = func(i, j int) {
+		if !inArea(i, j, m, n) {
+			return
+		}
+		if board[i][j] != 'O' {
+			return
+		}
+		board[i][j] = '*'
+		for k := 0; k < 4; k++ {
+			update(i+DirRow[k], j+DirCol[k])
+		}
+
+	}
+	// 从边界的 'O' 进行深度优先遍历
+
+	// 第一行和最后一行
+	for j := 0; j < n; j++ {
+		if board[0][j] == 'O' {
+			update(0, j)
+		}
+		if board[m-1][j] == 'O' {
+			update(m-1, j)
+		}
+	}
+	// 第一列和最后一列
+	for i := 0; i < m; i++ {
+		if board[i][0] == 'O' {
+			update(i, 0)
+		}
+		if board[i][n-1] == 'O' {
+			update(i, n-1)
+		}
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if board[i][j] == '*' {
+				board[i][j] = 'O'
+			} else {
+				board[i][j] = 'X'
+			}
+		}
+	}
+
+}
+
+var (
+	DirCol = []int{1, -1, 0, 0}
+	DirRow = []int{0, 0, 1, -1}
+)
+
+func inArea(row, col, rows, cols int) bool {
+	return row >= 0 && row < rows && col >= 0 && col < cols
+}
--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -2035,3 +2035,218 @@ func minWindow(s string, t string) string {
 
 	return s[start : start+minLen]
 }
+
+// 126. 单词接龙 II
+// 按字典 wordList 完成从单词 beginWord 到单词 endWord 转化，一个表示此过程的 转换序列 是形式上像 beginWord -> s1 -> s2 -> ... -> sk 这样的单词序列，并满足：
+//
+// 每对相邻的单词之间仅有单个字母不同。
+// 转换过程中的每个单词 si（1 <= i <= k）必须是字典 wordList 中的单词。注意，beginWord 不必是字典 wordList 中的单词。
+// sk == endWord
+// 给你两个单词 beginWord 和 endWord ，以及一个字典 wordList 。请你找出并返回所有从 beginWord 到 endWord 的 最短转换序列 ，如果不存在这样的转换序列，返回一个空列表。每个序列都应该以单词列表 [beginWord, s1, s2, ..., sk] 的形式返回。
+//
+// 示例 1：
+// 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+// 输出：[["hit","hot","dot","dog","cog"],["hit","hot","lot","log","cog"]]
+// 解释：存在 2 种最短的转换序列：
+// "hit" -> "hot" -> "dot" -> "dog" -> "cog"
+// "hit" -> "hot" -> "lot" -> "log" -> "cog"
+//
+// 示例 2：
+// 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+// 输出：[]
+// 解释：endWord "cog" 不在字典 wordList 中，所以不存在符合要求的转换序列。
+//
+// 提示：
+// 1 <= beginWord.length <= 7
+// endWord.length == beginWord.length
+// 1 <= wordList.length <= 5000
+// wordList[i].length == beginWord.length
+// beginWord、endWord 和 wordList[i] 由小写英文字母组成
+// beginWord != endWord
+// wordList 中的所有单词 互不相同
+func findLadders(beginWord string, endWord string, wordList []string) [][]string {
+
+	result := make([][]string, 0)
+
+	allComboDict := make(map[string][]string)
+	var builder strings.Builder
+	notEndWord := true
+	for _, word := range wordList {
+		if word == endWord {
+			notEndWord = false
+		}
+		for i := range word {
+			builder.Reset()
+			builder.WriteString(word[0:i])
+			builder.WriteString("*")
+			builder.WriteString(word[i+1:])
+			key := builder.String()
+			if _, ok := allComboDict[key]; !ok {
+				allComboDict[key] = make([]string, 0)
+			}
+			allComboDict[key] = append(allComboDict[key], word)
+		}
+	}
+	if notEndWord {
+		return result
+	}
+
+	// 广度优先遍历
+	queue := list.New()
+	queue.PushBack(beginWord)
+	visited := make(map[string]bool)
+	visited[beginWord] = true
+	step := 1
+	stepMap := make(map[string]int)
+	// 单词列表
+	from := make(map[string][]string)
+	found := false
+	for queue.Len() > 0 {
+		size := queue.Len()
+
+		for i := 0; i < size; i++ {
+
+			front := queue.Front()
+			queue.Remove(front)
+			word := front.Value.(string)
+
+			for j := range word {
+				builder.Reset()
+				builder.WriteString(word[0:j])
+				builder.WriteString("*")
+				builder.WriteString(word[j+1:])
+				key := builder.String()
+				if nextWords, ok := allComboDict[key]; ok {
+					for _, nextWord := range nextWords {
+						if stepMap[nextWord] == step {
+							from[nextWord] = append(from[nextWord], word)
+						}
+						if visited[nextWord] {
+							continue
+						}
+						visited[nextWord] = true
+						queue.PushBack(nextWord)
+						if nextWord == endWord {
+							found = true
+						}
+						if _, fromOk := from[nextWord]; !fromOk {
+							from[nextWord] = make([]string, 0)
+						}
+						from[nextWord] = append(from[nextWord], word)
+						stepMap[nextWord] = step
+					}
+				}
+			}
+
+		}
+		step++
+		// 已找到最短的
+		if found {
+			break
+		}
+	}
+	fmt.Println(from)
+
+	var dfs func(curWord string, idx int, path []string)
+
+	dfs = func(curWord string, idx int, path []string) {
+		if idx == 0 {
+			tmpPath := make([]string, step)
+			copy(tmpPath, path)
+			tmpPath[0] = beginWord
+			result = append(result, tmpPath)
+			return
+		}
+		for _, proWord := range from[curWord] {
+			path[idx] = proWord
+			dfs(proWord, idx-1, path)
+		}
+	}
+
+	if found {
+		path := make([]string, step)
+		path[step-1] = endWord
+		dfs(endWord, step-2, path)
+	}
+
+	return result
+}
+
+// 127. 单词接龙
+// 字典 wordList 中从单词 beginWord 和 endWord 的 转换序列 是一个按下述规格形成的序列：
+//
+// 序列中第一个单词是 beginWord 。
+// 序列中最后一个单词是 endWord 。
+// 每次转换只能改变一个字母。
+// 转换过程中的中间单词必须是字典 wordList 中的单词。
+// 给你两个单词 beginWord 和 endWord 和一个字典 wordList ，找到从 beginWord 到 endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0。
+//
+// 示例 1：
+// 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"] 输出：5
+// 解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+//
+// 示例 2：
+// 输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"] 输出：0
+// 解释：endWord "cog" 不在字典中，所以无法进行转换。
+//
+// 提示：
+// 1 <= beginWord.length <= 10
+// endWord.length == beginWord.length
+// 1 <= wordList.length <= 5000
+// wordList[i].length == beginWord.length
+// beginWord、endWord 和 wordList[i] 由小写英文字母组成
+// beginWord != endWord
+// wordList 中的所有字符串 互不相同
+func ladderLength(beginWord string, endWord string, wordList []string) int {
+	wordMap := make(map[string]bool)
+	for _, word := range wordList {
+		wordMap[word] = true
+	}
+	if _, ok := wordMap[endWord]; !ok {
+		return 0
+	}
+	allComboDict := make(map[string][]string)
+	for _, word := range wordList {
+		for i := range word {
+			key := word[0:i] + "*" + word[i+1:]
+			if _, ok := allComboDict[key]; !ok {
+				allComboDict[key] = make([]string, 0)
+			}
+			allComboDict[key] = append(allComboDict[key], word)
+		}
+	}
+	// 广度优先遍历
+	queue := list.New()
+	queue.PushBack(beginWord)
+
+	visited := make(map[string]bool)
+	visited[beginWord] = true
+	step := 1
+	for queue.Len() > 0 {
+		size := queue.Len()
+		for i := 0; i < size; i++ {
+
+			front := queue.Front()
+			queue.Remove(front)
+			word := front.Value.(string)
+			if word == endWord {
+				return step
+			}
+			for j := range word {
+				key := word[0:j] + "*" + word[j+1:]
+				if nextWords, ok := allComboDict[key]; ok {
+					for _, nextWord := range nextWords {
+						if visited[nextWord] == false {
+							queue.PushBack(nextWord)
+							visited[nextWord] = true
+						}
+					}
+				}
+			}
+
+		}
+		step++
+	}
+
+	return 0
+}
