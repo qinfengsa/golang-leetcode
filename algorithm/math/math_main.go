817ea872fe69cd187a2349dfbc9d10d050bb7e91
qinfengsa <15800012123@163.com>
2021-09-21 13:12:30 +0800

    LeetCode 2021-09-21 160. 相交链表 162. 寻找峰值 164. 最大间距 165. 比较版本号 166. 分数到小数

--- algorithm/math/math_main.go
+++ algorithm/math/math_main.go
@@ -1229,3 +1229,78 @@ func maxPoints(points [][]int) int {
 
 	return maxCnt
 }
+
+// 166. 分数到小数
+// 给定两个整数，分别表示分数的分子 numerator 和分母 denominator，以 字符串形式返回小数 。
+// 如果小数部分为循环小数，则将循环的部分括在括号内。
+// 如果存在多个答案，只需返回 任意一个 。
+// 对于所有给定的输入，保证 答案字符串的长度小于 104 。
+//
+// 示例 1：
+// 输入：numerator = 1, denominator = 2 输出："0.5"
+//
+// 示例 2：
+// 输入：numerator = 2, denominator = 1 输出："2"
+//
+// 示例 3：
+// 输入：numerator = 2, denominator = 3 输出："0.(6)"
+//
+// 示例 4：
+// 输入：numerator = 4, denominator = 333 输出："0.(012)"
+//
+// 示例 5：
+// 输入：numerator = 1, denominator = 5 输出："0.2"
+//
+// 提示：
+// -231 <= numerator, denominator <= 231 - 1
+// denominator != 0
+func fractionToDecimal(numerator int, denominator int) string {
+	if numerator == 0 {
+		return "0"
+	}
+	if denominator == 1 {
+		return fmt.Sprintf("%d", numerator)
+	}
+	if denominator == -1 {
+		if numerator >= 0 {
+			return strconv.Itoa(-numerator)
+		} else {
+			result := strconv.Itoa(numerator)
+			return result[1:]
+		}
+	}
+	result := ""
+	if numerator*denominator < 0 {
+		result += "-"
+	}
+	numerator, denominator = abs(numerator), abs(denominator)
+	// 商
+	quotient := numerator / denominator
+	remainder := numerator % denominator
+	result += strconv.Itoa(quotient)
+	if remainder == 0 {
+		return result
+	}
+	result += "."
+	hash := make(map[int]int)
+
+	for remainder != 0 {
+		hash[remainder] = len(result)
+		remainder *= 10
+		result += strconv.Itoa(remainder / denominator)
+		remainder %= denominator
+		if i, ok := hash[remainder]; ok {
+			result = result[0:i] + "(" + result[i:] + ")"
+			break
+		}
+	}
+
+	return result
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -1 * x
+	}
+	return x
+}
