0f130f06aaeae85307b09f57a5616e0db09d315d
qinfengsa <15800012123@163.com>
2021-09-23 10:51:17 +0800

    LeetCode 2021-09-23 173. 二叉搜索树迭代器 174. 地下城游戏 179. 最大数 187. 重复的DNA序列 188. 买卖股票的最佳时机 IV

--- data-structure/hash/hash_main.go
+++ data-structure/hash/hash_main.go
@@ -603,3 +603,32 @@ func groupAnagrams(strs []string) [][]string {
 
 	return result
 }
+
+// 187. 重复的DNA序列
+// 所有 DNA 都由一系列缩写为 'A'，'C'，'G' 和 'T' 的核苷酸组成，例如："ACGAATTCCG"。在研究 DNA 时，识别 DNA 中的重复序列有时会对研究非常有帮助。
+//
+// 编写一个函数来找出所有目标子串，目标子串的长度为 10，且在 DNA 字符串 s 中出现次数超过一次。
+//
+// 示例 1：
+// 输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+// 输出：["AAAAACCCCC","CCCCCAAAAA"]
+//
+// 示例 2：
+// 输入：s = "AAAAAAAAAAAAA"
+// 输出：["AAAAAAAAAA"]
+//
+// 提示：
+// 0 <= s.length <= 105
+// s[i] 为 'A'、'C'、'G' 或 'T'
+func findRepeatedDnaSequences(s string) []string {
+	countMap := make(map[string]int)
+	result := make([]string, 0)
+	for i := 0; i <= len(s)-10; i++ {
+		str := s[i : i+10]
+		if count, ok := countMap[str]; ok && count == 1 {
+			result = append(result, str)
+		}
+		countMap[str]++
+	}
+	return result
+}
