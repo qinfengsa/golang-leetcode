37b11cbccc93130b35b489011c7d3d8a8c1f4781
qinfengsa <15800012123@163.com>
2021-09-24 09:17:12 +0800

    LeetCode 2021-09-24 189. 旋转数组 190. 颠倒二进制位 191. 位1的个数 198. 打家劫舍 199. 二叉树的右视图

--- data-structure/tree/tree_main.go
+++ data-structure/tree/tree_main.go
@@ -1887,3 +1887,48 @@ func postorderTraversal(root *TreeNode) []int {
 
 	return result
 }
+
+// 199. 二叉树的右视图
+// 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+//
+// 示例 1:
+// 输入: [1,2,3,null,5,null,4]  输出: [1,3,4]
+//
+// 示例 2:
+// 输入: [1,null,3] 输出: [1,3]
+//
+// 示例 3:
+// 输入: [] 输出: []
+//
+// 提示:
+// 二叉树的节点个数的范围是 [0,100]
+// -100 <= Node.val <= 100
+func rightSideView(root *TreeNode) []int {
+	result := make([]int, 0)
+	if root == nil {
+		return result
+	}
+	// 广度优先遍历
+	queue := list.New()
+	queue.PushBack(root)
+
+	for queue.Len() > 0 {
+		n := queue.Len()
+		val := 0
+		for i := 0; i < n; i++ {
+			front := queue.Front()
+			queue.Remove(front)
+			node := front.Value.(*TreeNode)
+			val = node.Val
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+		}
+		result = append(result, val)
+	}
+
+	return result
+}
