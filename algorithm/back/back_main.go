e6348c57faa60143330ff7a70a5c15688c762509
qinfengsa <15800012123@163.com>
2021-10-07 09:10:49 +0800

    LeetCode 2021-10-07 282. 给表达式添加运算符 283. 移动零 287. 寻找重复数 289. 生命游戏 290. 单词规律 434. 字符串中的单词数

--- algorithm/back/back_main.go
+++ algorithm/back/back_main.go
@@ -941,3 +941,69 @@ func combinationSum3(k int, n int) [][]int {
 	back(n, 1, 0, make([]int, k))
 	return result
 }
+
+// 282. 给表达式添加运算符
+// 给定一个仅包含数字 0-9 的字符串 num 和一个目标值整数 target ，在 num 的数字之间添加 二元 运算符（不是一元）+、- 或 * ，返回所有能够得到目标值的表达式。
+//
+// 示例 1:
+// 输入: num = "123", target = 6
+// 输出: ["1+2+3", "1*2*3"]
+//
+// 示例 2:
+// 输入: num = "232", target = 8
+// 输出: ["2*3+2", "2+3*2"]
+//
+// 示例 3:
+// 输入: num = "105", target = 5
+// 输出: ["1*0+5","10-5"]
+//
+// 示例 4:
+// 输入: num = "00", target = 0
+// 输出: ["0+0", "0-0", "0*0"]
+//
+// 示例 5:
+// 输入: num = "3456237490", target = 9191
+// 输出: []
+//
+// 提示：
+// 1 <= num.length <= 10
+// num 仅含数字
+// -231 <= target <= 231 - 1
+func addOperators(num string, target int) []string {
+	result := make([]string, 0)
+	n := len(num)
+
+	var back func(start, total, lastNum int, str string)
+
+	back = func(start, total, lastNum int, str string) {
+		if start == n {
+			if total == target {
+				result = append(result, str)
+			}
+			return
+		}
+		for i := start; i < n; i++ {
+			if i > start && num[start] == '0' {
+				break
+			}
+			curNum, _ := strconv.Atoi(num[start : i+1])
+			if start == 0 {
+				back(i+1, curNum, curNum, str+strconv.Itoa(curNum))
+			} else {
+				// +
+				back(i+1, total+curNum, curNum, str+"+"+strconv.Itoa(curNum))
+				// -
+				back(i+1, total-curNum, -curNum, str+"-"+strconv.Itoa(curNum))
+				// *
+				back(
+					i+1,
+					total-lastNum+lastNum*curNum,
+					lastNum*curNum,
+					str+"*"+strconv.Itoa(curNum))
+			}
+		}
+	}
+	back(0, 0, 0, "")
+
+	return result
+}
