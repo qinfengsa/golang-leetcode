0d5b078da74475156f9f455613d02c6ff3f8746a
qinfengsa <15800012123@163.com>
2021-11-01 10:31:45 +0800

    LeetCode 2021-11-01 450. 删除二叉搜索树中的节点 449. 序列化和反序列化二叉搜索树 452. 用最少数量的箭引爆气球 454. 四数相加 II 455. 分发饼干 575. 分糖果

--- data-structure/hash/hash_main.go
+++ data-structure/hash/hash_main.go
@@ -360,14 +360,14 @@ out:
 // 数组的长度为[2, 10,000]，并且确定为偶数。
 // 数组中数字的大小在范围[-100,000, 100,000]内。
 func distributeCandies(candies []int) int {
-	helf := len(candies) >> 1
+	half := len(candies) >> 1
 	candyMap := map[int]bool{}
-	for _, cand := range candies {
-		candyMap[cand] = true
+	for _, candy := range candies {
+		candyMap[candy] = true
 	}
 	size := len(candyMap)
-	if size >= helf {
-		return helf
+	if size >= half {
+		return half
 	}
 	return size
 }
@@ -1085,5 +1085,51 @@ func numberOfBoomerangs(points [][]int) int {
 
 		}
 	}
+	return result
+}
+
+// 454. 四数相加 II
+// 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+//
+// 0 <= i, j, k, l < n
+// nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+//
+// 示例 1：
+// 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+// 输出：2
+// 解释：
+// 两个元组如下：
+// 1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+// 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+//
+// 示例 2：
+// 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+// 输出：1
+//
+// 提示：
+// n == nums1.length
+// n == nums2.length
+// n == nums3.length
+// n == nums4.length
+// 1 <= n <= 200
+// -2^28 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2^28
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	numMap := make(map[int]int)
+	result := 0
+	for _, num1 := range nums1 {
+		for _, num2 := range nums2 {
+			numMap[num1+num2]++
+		}
+	}
+
+	for _, num3 := range nums3 {
+		for _, num4 := range nums4 {
+			num := -num3 - num4
+			if count, ok := numMap[num]; ok {
+				result += count
+			}
+		}
+	}
+
 	return result
 }
