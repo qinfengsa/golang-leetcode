c2c70833c5225c5d06ae636e23fafc708c30bf3a
qinfengsa <15800012123@163.com>
2021-11-24 13:35:08 +0800

    LeetCode 2021-11-24 530. 二叉搜索树的最小绝对差 532. 数组中的 k-diff 数对 535. TinyURL 的加密与解密 537. 复数乘法 538. 把二叉搜索树转换为累加树

--- algorithm/math/math_main.go
+++ algorithm/math/math_main.go
@@ -2577,3 +2577,39 @@ func smallestGoodBase(n string) string {
 	}
 	return strconv.Itoa(num - 1)
 }
+
+// 537. 复数乘法
+// 复数 可以用字符串表示，遵循 "实部+虚部i" 的形式，并满足下述条件：
+//
+// 实部 是一个整数，取值范围是 [-100, 100]
+// 虚部 也是一个整数，取值范围是 [-100, 100]
+// i2 == -1
+// 给你两个字符串表示的复数 num1 和 num2 ，请你遵循复数表示形式，返回表示它们乘积的字符串。
+//
+// 示例 1：
+// 输入：num1 = "1+1i", num2 = "1+1i"
+// 输出："0+2i"
+// 解释：(1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i ，你需要将它转换为 0+2i 的形式。
+//
+// 示例 2：
+// 输入：num1 = "1+-1i", num2 = "1+-1i"
+// 输出："0+-2i"
+// 解释：(1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i ，你需要将它转换为 0+-2i 的形式。
+//
+// 提示：
+// num1 和 num2 都是有效的复数表示。
+func complexNumberMultiply(num1 string, num2 string) string {
+	var num1Real, num1Imag, num2Real, num2Imag int
+
+	_, err := fmt.Sscanf(num1, "%d+%di", &num1Real, &num1Imag)
+	if err != nil {
+		return ""
+	}
+	_, err = fmt.Sscanf(num2, "%d+%di", &num2Real, &num2Imag)
+	if err != nil {
+		return ""
+	}
+
+	return fmt.Sprintf("%d+%di", num1Real*num2Real-num1Imag*num2Imag,
+		num1Real*num2Imag+num1Imag*num2Real)
+}
---
+++ data-structure/leetcode/codec/tiny_url.go
@@ -0,0 +1,52 @@
+package codec
+
+import (
+	"math/rand"
+	"strings"
+)
+
+// TinyUrlCodec 535. TinyURL 的加密与解密
+// TinyURL是一种URL简化服务，
+// 比如：当你输入一个URL https://leetcode.com/problems/design-tinyurl 时，
+// 它将返回一个简化的URL http://tinyurl.com/4e9iAk.
+//
+// 要求：设计一个 TinyURL 的加密 encode 和解密 decode 的方法。
+// 你的加密和解密算法如何设计和运作是没有限制的，你只需要保证一个URL可以被加密成一个TinyURL，
+// 并且这个TinyURL可以用解密方法恢复成原本的URL。
+type TinyUrlCodec struct {
+	urlMap map[string]string
+}
+
+var (
+	alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
+
+func getRandStr() string {
+	var builder strings.Builder
+	for i := 0; i < 6; i++ {
+		builder.WriteByte(alphabet[rand.Intn(62)])
+	}
+	return builder.String()
+}
+
+func ConstructorTinyUrl() TinyUrlCodec {
+	return TinyUrlCodec{urlMap: make(map[string]string)}
+}
+
+// Encodes a URL to a shortened URL.
+func (this *TinyUrlCodec) encode(longUrl string) string {
+	key := getRandStr()
+	_, ok := this.urlMap[key]
+	for ok {
+		key = getRandStr()
+		_, ok = this.urlMap[key]
+	}
+	this.urlMap[key] = longUrl
+	return "http://tinyurl.com/" + key
+}
+
+// Decodes a shortened URL to its original URL.
+func (this *TinyUrlCodec) decode(shortUrl string) string {
+	key := strings.ReplaceAll(shortUrl, "http://tinyurl.com/", "")
+	return this.urlMap[key]
+}
