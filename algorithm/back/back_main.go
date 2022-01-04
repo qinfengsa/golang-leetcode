d27e9159586bde7e5ec7cc0c08f7c3b28c085c0b
qinfengsa <15800012123@163.com>
2022-01-04 18:44:51 +0800

    LeetCode 2022-01-04 633. 平方数之和 636. 函数的独占时间 637. 二叉树的层平均值 638. 大礼包 1995. 统计特殊四元组

--- algorithm/back/back_main.go
+++ algorithm/back/back_main.go
@@ -2,6 +2,7 @@ package back
 
 import (
 	"fmt"
+	"math"
 	"sort"
 	"strconv"
 	"strings"
@@ -1341,4 +1342,109 @@ func countArrangement(n int) int {
 	}
 	back(1)
 	return result
+}
+
+// 638. 大礼包
+// 在 LeetCode 商店中， 有 n 件在售的物品。每件物品都有对应的价格。然而，也有一些大礼包，每个大礼包以优惠的价格捆绑销售一组物品。
+//
+// 给你一个整数数组 price 表示物品价格，其中 price[i] 是第 i 件物品的价格。另有一个整数数组 needs 表示购物清单，其中 needs[i] 是需要购买第 i 件物品的数量。
+//
+// 还有一个数组 special 表示大礼包，special[i] 的长度为 n + 1 ，其中 special[i][j] 表示第 i 个大礼包中内含第 j 件物品的数量，且 special[i][n] （也就是数组中的最后一个整数）为第 i 个大礼包的价格。
+//
+// 返回 确切 满足购物清单所需花费的最低价格，你可以充分利用大礼包的优惠活动。你不能购买超出购物清单指定数量的物品，即使那样会降低整体价格。任意大礼包可无限次购买。
+//
+// 示例 1：
+// 输入：price = [2,5], special = [[3,0,5],[1,2,10]], needs = [3,2]
+// 输出：14
+// 解释：有 A 和 B 两种物品，价格分别为 ¥2 和 ¥5 。
+// 大礼包 1 ，你可以以 ¥5 的价格购买 3A 和 0B 。
+// 大礼包 2 ，你可以以 ¥10 的价格购买 1A 和 2B 。
+// 需要购买 3 个 A 和 2 个 B ， 所以付 ¥10 购买 1A 和 2B（大礼包 2），以及 ¥4 购买 2A 。
+//
+// 示例 2：
+// 输入：price = [2,3,4], special = [[1,1,0,4],[2,2,1,9]], needs = [1,2,1]
+// 输出：11
+// 解释：A ，B ，C 的价格分别为 ¥2 ，¥3 ，¥4 。
+// 可以用 ¥4 购买 1A 和 1B ，也可以用 ¥9 购买 2A ，2B 和 1C 。
+// 需要买 1A ，2B 和 1C ，所以付 ¥4 买 1A 和 1B（大礼包 1），以及 ¥3 购买 1B ， ¥4 购买 1C 。
+// 不可以购买超出待购清单的物品，尽管购买大礼包 2 更加便宜。
+//
+// 提示：
+// n == price.length
+// n == needs.length
+// 1 <= n <= 6
+// 0 <= price[i] <= 10
+// 0 <= needs[i] <= 10
+// 1 <= special.length <= 100
+// special[i].length == n + 1
+// 0 <= special[i][j] <= 50
+func shoppingOffers(price []int, special [][]int, needs []int) int {
+	result := math.MaxInt32
+	sum := 0
+	n := len(needs)
+	valid := func() bool {
+		for _, need := range needs {
+			if need < 0 {
+				return false
+			}
+		}
+		return true
+	}
+	// 计算需要几个特别礼包
+	calSpecialNum := func(curSpecial, needs []int) int {
+		maxNum := math.MaxInt32
+		for i := 0; i < len(needs); i++ {
+			if needs[i] < curSpecial[i] {
+				return 0
+			}
+			if curSpecial[i] != 0 {
+				num := needs[i] / curSpecial[i]
+				maxNum = min(maxNum, num)
+			}
+		}
+		return maxNum
+	}
+
+	var back func(start int)
+
+	back = func(start int) {
+		if !valid() {
+			return
+		}
+		tmpSum := sum
+		for i := 0; i < len(needs); i++ {
+			sum += needs[i] * price[i]
+		}
+		result = min(result, sum)
+		sum = tmpSum
+		for i := start; i < len(special); i++ {
+			curSpec := special[i]
+			// 特别礼包数量
+			specNum := calSpecialNum(curSpec, needs)
+
+			for j := 1; j <= specNum; j++ {
+				tmpNeeds := make([]int, n)
+				copy(tmpNeeds, needs)
+				for k := 0; k < n; k++ {
+					needs[k] -= j * curSpec[k]
+				}
+				// curSpec[n]
+				sum += curSpec[n] * j
+				back(i + 1)
+				needs = tmpNeeds
+				sum = tmpSum
+			}
+
+		}
+	}
+
+	back(0)
+
+	return result
+}
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
 }
--- algorithm/math/math_main.go
+++ algorithm/math/math_main.go
@@ -2834,3 +2834,48 @@ func dayOfYear(date string) int {
 	}
 	return result
 }
+
+// 633. 平方数之和
+// 给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。
+//
+// 示例 1：
+// 输入：c = 5 输出：true
+// 解释：1 * 1 + 2 * 2 = 5
+//
+// 示例 2：
+// 输入：c = 3 输出：false
+//
+// 示例 3：
+// 输入：c = 4 输出：true
+//
+// 示例 4：
+// 输入：c = 2 输出：true
+//
+// 示例 5：
+// 输入：c = 1 输出：true
+//
+// 提示：
+// 0 <= c <= 231 - 1
+func judgeSquareSum(c int) bool {
+	// 费马平方和定理告诉我们：
+	//
+	// 一个非负整数 cc 能够表示为两个整数的平方和，当且仅当 cc 的所有形如 4k+34k+3 的质因子的幂次均为偶数。
+	//
+	// 证明方法可以见 这里。
+	//
+	// 因此我们对 c 进行质因数分解，再判断形如 4k+3 的质因子的幂次是否均为偶数即可。
+	if c < 3 {
+		return true
+	}
+
+	for i := 1; i*i <= c; i++ {
+		num := c - i*i
+		b := math.Sqrt(float64(num))
+		if b == math.Floor(b) {
+			return true
+		}
+
+	}
+
+	return false
+}
