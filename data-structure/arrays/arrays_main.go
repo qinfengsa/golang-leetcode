2971df78b302f86b46b1267e4541d24b714d8e95
qinfengsa <15800012123@163.com>
2022-01-28 10:39:08 +0800

    LeetCode 2022-01-28 674. 最长连续递增序列 680. 验证回文字符串 Ⅱ 682. 棒球比赛 1996. 游戏中弱角色的数量 2047. 句子中的有效单词数

--- data-structure/arrays/arrays_main.go
+++ data-structure/arrays/arrays_main.go
@@ -1013,18 +1013,18 @@ func findLengthOfLCIS(nums []int) int {
 	if size == 0 {
 		return 0
 	}
-	max, count := 1, 1
+	maxCnt, count := 1, 1
 	for i := 1; i < size; i++ {
 		if nums[i] > nums[i-1] {
 			count++
 		} else {
 			count = 1
 		}
-		if count > max {
-			max = count
+		if count > maxCnt {
+			maxCnt = count
 		}
 	}
-	return max
+	return maxCnt
 }
 
 // 697. 数组的度
@@ -4530,4 +4530,52 @@ func constructArray(n int, k int) []int {
 		k++
 	}
 	return nums
+}
+
+// 1996. 游戏中弱角色的数量
+// 你正在参加一个多角色游戏，每个角色都有两个主要属性：攻击 和 防御 。给你一个二维整数数组 properties ，其中 properties[i] = [attacki, defensei] 表示游戏中第 i 个角色的属性。
+//
+// 如果存在一个其他角色的攻击和防御等级 都严格高于 该角色的攻击和防御等级，则认为该角色为 弱角色 。更正式地，如果认为角色 i 弱于 存在的另一个角色 j ，那么 attackj > attacki 且 defensej > defensei 。
+//
+// 返回 弱角色 的数量。
+//
+// 示例 1：
+// 输入：properties = [[5,5],[6,3],[3,6]]
+// 输出：0
+// 解释：不存在攻击和防御都严格高于其他角色的角色。
+//
+// 示例 2：
+// 输入：properties = [[2,2],[3,3]]
+// 输出：1
+// 解释：第一个角色是弱角色，因为第二个角色的攻击和防御严格大于该角色。
+//
+// 示例 3：
+// 输入：properties = [[1,5],[10,4],[4,3]]
+// 输出：1
+// 解释：第三个角色是弱角色，因为第二个角色的攻击和防御严格大于该角色。
+//
+// 提示：
+// 2 <= properties.length <= 105
+// properties[i].length == 2
+// 1 <= attacki, defensei <= 105
+func numberOfWeakCharacters(properties [][]int) int {
+	// 攻击 降序 防御 升序  然后使用栈 找出 最大的
+	sort.Slice(properties, func(i, j int) bool {
+		if properties[i][0] == properties[j][0] {
+			return properties[i][1] < properties[j][1]
+		} else {
+			return properties[i][0] > properties[j][0]
+		}
+	})
+	n := len(properties)
+	count := 0
+	maxDef := properties[0][1]
+	for i := 1; i < n; i++ {
+		if properties[i][1] < maxDef {
+			count++
+		} else if properties[i][1] > maxDef {
+			maxDef = properties[i][1]
+		}
+	}
+	return count
 }
