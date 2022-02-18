ba1e56587e98dc28a23288d6ca64611c859f3686
qinfengsa <15800012123@163.com>
2022-02-18 14:18:27 +0800

    LeetCode 2022-02-18 697. 数组的度 698. 划分为k个相等的子集 699. 掉落的方块 701. 二叉搜索树中的插入操作 1791. 找出星型图的中心节点

--- algorithm/back/back_main.go
+++ algorithm/back/back_main.go
@@ -1448,3 +1448,63 @@ func min(x, y int) int {
 	}
 	return y
 }
+
+// 698. 划分为k个相等的子集
+// 给定一个整数数组  nums 和一个正整数 k，找出是否有可能把这个数组分成 k 个非空子集，其总和都相等。
+//
+// 示例 1：
+// 输入： nums = [4, 3, 2, 3, 5, 2, 1], k = 4
+// 输出： True
+// 说明： 有可能将其分成 4 个子集（5），（1,4），（2,3），（2,3）等于总和。
+//
+// 提示：
+// 1 <= k <= len(nums) <= 16
+// 0 < nums[i] < 10000
+func canPartitionKSubsets(nums []int, k int) bool {
+	sum, maxVal := 0, 0
+	for _, num := range nums {
+		sum += num
+		maxVal = max(maxVal, num)
+	}
+	if sum%k != 0 {
+		return false
+	}
+	target := sum / k
+	if maxVal > target {
+		return false
+	}
+	sort.Ints(nums)
+	n := len(nums)
+	visited := make([]bool, n)
+
+	var back func(count, num, index int) bool
+
+	back = func(count, num, index int) bool {
+		if count == 0 {
+			return true
+		}
+		if num == target {
+			return back(count-1, 0, 0)
+		}
+
+		for i := index; i < n; i++ {
+			if visited[i] {
+				continue
+			}
+			if num+nums[i] > target {
+				continue
+			}
+			visited[i] = true
+			if back(count, num+nums[i], i+1) {
+				return true
+			}
+			visited[i] = false
+			for i < n-1 && nums[i] == nums[i+1] {
+				i++
+			}
+		}
+		return false
+	}
+
+	return back(k, 0, 0)
+}
--- data-structure/arrays/arrays_main.go
+++ data-structure/arrays/arrays_main.go
@@ -1044,8 +1044,7 @@ func findLengthOfLCIS(nums []int) int {
 // 输入: [1,2,2,3,1,4,2]
 // 输出: 6
 func findShortestSubArray(nums []int) int {
-	size := len(nums)
-	max, result := 0, size
+	maxVal, result := 0, len(nums)
 	countMap := map[int]int{}
 	indexMap := map[int]int{}
 	for i, num := range nums {
@@ -1055,12 +1054,12 @@ func findShortestSubArray(nums []int) int {
 		}
 		count++
 		countMap[num] = count
-		length := i - indexMap[num] + 1
-		if count > max {
-			max = count
-			result = length
-		} else if count == max && length < result {
-			result = length
+		size := i - indexMap[num] + 1
+		if count > maxVal {
+			maxVal = count
+			result = size
+		} else if count == maxVal && size < result {
+			result = size
 		}
 
 	}
@@ -4731,6 +4730,82 @@ func maxSumOfThreeSubarrays(nums []int, k int) []int {
 			result[1] = i
 			result[2] = rightIdx
 		}
+	}
+
+	return result
+}
+
+// 699. 掉落的方块
+// 在无限长的数轴（即 x 轴）上，我们根据给定的顺序放置对应的正方形方块。
+// 第 i 个掉落的方块（positions[i] = (left, side_length)）是正方形，其中 left 表示该方块最左边的点位置(positions[i][0])，side_length 表示该方块的边长(positions[i][1])。
+// 每个方块的底部边缘平行于数轴（即 x 轴），并且从一个比目前所有的落地方块更高的高度掉落而下。在上一个方块结束掉落，并保持静止后，才开始掉落新方块。
+// 方块的底边具有非常大的粘性，并将保持固定在它们所接触的任何长度表面上（无论是数轴还是其他方块）。邻接掉落的边不会过早地粘合在一起，因为只有底边才具有粘性。
+//
+// 返回一个堆叠高度列表 ans 。每一个堆叠高度 ans[i] 表示在通过 positions[0], positions[1], ..., positions[i] 表示的方块掉落结束后，目前所有已经落稳的方块堆叠的最高高度。
+//
+// 示例 1:
+// 输入: [[1, 2], [2, 3], [6, 1]]
+// 输出: [2, 5, 5]
+// 解释:
+// 第一个方块 positions[0] = [1, 2] 掉落：
+// _aa
+// _aa
+// -------
+// 方块最大高度为 2 。
+//
+// 第二个方块 positions[1] = [2, 3] 掉落：
+// __aaa
+// __aaa
+// __aaa
+// _aa__
+// _aa__
+// --------------
+// 方块最大高度为5。
+// 大的方块保持在较小的方块的顶部，不论它的重心在哪里，因为方块的底部边缘有非常大的粘性。
+//
+// 第三个方块 positions[1] = [6, 1] 掉落：
+// __aaa
+// __aaa
+// __aaa
+// _aa
+// _aa___a
+// --------------
+// 方块最大高度为5。
+//
+// 因此，我们返回结果[2, 5, 5]。
+//
+// 示例 2:
+// 输入: [[100, 100], [200, 100]]
+// 输出: [100, 100]
+// 解释: 相邻的方块不会过早地卡住，只有它们的底部边缘才能粘在表面上。
+//
+// 注意:
+// 1 <= positions.length <= 1000.
+// 1 <= positions[i][0] <= 10^8.
+// 1 <= positions[i][1] <= 10^6.
+func fallingSquares(positions [][]int) []int {
+	n := len(positions)
+	result := make([]int, n)
+	maxHeight := positions[0][1]
+	result[0] = maxHeight
+	if n == 1 {
+		return result
+	}
+	heights := make([]int, n)
+	heights[0] = maxHeight
+
+	for i := 1; i < n; i++ {
+		height := positions[i][1]
+		for j := i - 1; j >= 0; j-- {
+			if positions[j][0]+positions[j][1] <= positions[i][0] ||
+				positions[i][0]+positions[i][1] <= positions[j][0] {
+				continue
+			}
+			height = max(height, heights[j]+positions[i][1])
+		}
+		heights[i] = height
+		maxHeight = max(maxHeight, height)
+		result[i] = maxHeight
 	}
 
 	return result
