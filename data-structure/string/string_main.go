a95a010a6695d0a79342fed0e2b8aa6aa15b2683
qinfengsa <15800012123@163.com>
2022-02-23 14:21:28 +0800

    LeetCode 2022-02-23 713. 乘积小于K的子数组 714. 买卖股票的最佳时机含手续费 917. 仅仅反转字母

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -3915,3 +3915,47 @@ func reversePrefix(word string, ch byte) string {
 	}
 	return string(chars)
 }
+
+// 917. 仅仅反转字母
+// 给你一个字符串 s ，根据下述规则反转字符串：
+//
+// 所有非英文字母保留在原有位置。
+// 所有英文字母（小写或大写）位置反转。
+// 返回反转后的 s 。
+//
+// 示例 1：
+// 输入：s = "ab-cd"
+// 输出："dc-ba"
+//
+// 示例 2：
+// 输入：s = "a-bC-dEf-ghIj"
+// 输出："j-Ih-gfE-dCba"
+//
+// 示例 3：
+// 输入：s = "Test1ng-Leet=code-Q!"
+// 输出："Qedo1ct-eeLg=ntse-T!"
+//
+// 提示
+// 1 <= s.length <= 100
+// s 仅由 ASCII 值在范围 [33, 122] 的字符组成
+// s 不含 '\"' 或 '\\'
+func reverseOnlyLetters(s string) string {
+	chars := []byte(s)
+	n := len(s)
+	left, right := 0, n-1
+	for left < right {
+		if !unicode.IsLetter(rune(chars[left])) {
+			left++
+			continue
+		}
+		if !unicode.IsLetter(rune(chars[right])) {
+			right--
+			continue
+		}
+		chars[left], chars[right] = chars[right], chars[left]
+		left++
+		right--
+	}
+
+	return string(chars)
+}
