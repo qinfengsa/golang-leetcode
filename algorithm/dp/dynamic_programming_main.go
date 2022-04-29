000f6edbe15b3048f2b2e3401effc331b0946219
wangheng <15800012123@163.com>
2022-04-29 15:37:19 +0800

    LeetCode 2022-04-29 730. 统计不同回文子序列 733. 图像渲染 883. 三维形体投影面积 905. 按奇偶排序数组

--- algorithm/dp/dynamic_programming_main.go
+++ algorithm/dp/dynamic_programming_main.go
@@ -3270,3 +3270,74 @@ func findLength(nums1 []int, nums2 []int) int {
 	}
 	return result
 }
+
+// 730. 统计不同回文子序列
+// 给定一个字符串 s，返回 s 中不同的非空「回文子序列」个数 。
+//
+// 通过从 s 中删除 0 个或多个字符来获得子序列。
+//
+// 如果一个字符序列与它反转后的字符序列一致，那么它是「回文字符序列」。
+//
+// 如果有某个 i , 满足 ai != bi ，则两个序列 a1, a2, ... 和 b1, b2, ... 不同。
+//
+// 注意：
+// 结果可能很大，你需要对 109 + 7 取模 。
+//
+// 示例 1：
+// 输入：s = 'bccb'
+// 输出：6
+// 解释：6 个不同的非空回文子字符序列分别为：'b', 'c', 'bb', 'cc', 'bcb', 'bccb'。
+// 注意：'bcb' 虽然出现两次但仅计数一次。
+//
+// 示例 2：
+// 输入：s = 'abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba'
+// 输出：104860361
+// 解释：共有 3104860382 个不同的非空回文子序列，104860361 对 109 + 7 取模后的值。
+//
+// 提示：
+// 1 <= s.length <= 1000
+// s[i] 仅包含 'a', 'b', 'c' 或 'd'
+func countPalindromicSubsequences(s string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+		dp[i][i] = 1
+	}
+
+	for i := n - 2; i >= 0; i-- {
+		for j := i + 1; j < n; j++ {
+			if s[i] == s[j] {
+				// i+1 ~ j-1 之间的回文字符串 + s[i] s[j]
+				dp[i][j] = dp[i+1][j-1]<<1 + 2
+
+				// 去除重复的 和 s[i] 相同的字符
+				l, r := i+1, j-1
+				c := s[i]
+				for l <= r && s[l] != c {
+					l++
+				}
+				for l <= r && s[r] != c {
+					r--
+				}
+				if l == r {
+					// 只有一个 c
+					dp[i][j]--
+				} else if l < r {
+					// 2 个 c 减去 l + 1 ~ r -1 之间的 回文
+					dp[i][j] -= dp[l+1][r-1] + 2
+				}
+
+			} else {
+				dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1]
+			}
+			if dp[i][j] >= 0 {
+				dp[i][j] %= Mod
+			} else {
+				dp[i][j] += Mod
+			}
+
+		}
+	}
+	return dp[0][n-1]
+}
