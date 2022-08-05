5a210f74840b8aa881f98a46b31aa5e2d21b5f10
wangheng <15800012123@163.com>
2022-08-05 12:04:53 +0800

    LeetCode 2022-08-05 743. 网络延迟时间 744. 寻找比目标字母大的最小字母

--- data-structure/graph/graph_main.go
+++ data-structure/graph/graph_main.go
@@ -2,6 +2,7 @@ package graph
 
 import (
 	"container/list"
+	"math"
 	"sort"
 )
 
@@ -862,4 +863,88 @@ func sequenceReconstruction(nums []int, sequences [][]int) bool {
 		}
 	}
 	return true
+}
+
+// 743. 网络延迟时间
+// 有 n 个网络节点，标记为 1 到 n。
+//
+// 给你一个列表 times，表示信号经过 有向 边的传递时间。 times[i] = (ui, vi, wi)，其中 ui 是源节点，vi 是目标节点， wi 是一个信号从源节点传递到目标节点的时间。
+//
+// 现在，从某个节点 K 发出一个信号。需要多久才能使所有节点都收到信号？如果不能使所有节点收到信号，返回 -1 。
+//
+// 示例 1：
+// 输入：times = [[2,1,1],[2,3,1],[3,4,1]], n = 4, k = 2
+// 输出：2
+//
+// 示例 2：
+// 输入：times = [[1,2,1]], n = 2, k = 1
+// 输出：1
+//
+// 示例 3：
+// 输入：times = [[1,2,1]], n = 2, k = 2
+// 输出：-1
+//
+// 提示：
+// 1 <= k <= n <= 100
+// 1 <= times.length <= 6000
+// times[i].length == 3
+// 1 <= ui, vi <= n
+// ui != vi
+// 0 <= wi <= 100
+// 所有 (ui, vi) 对都 互不相同（即，不含重复边）
+func networkDelayTime(times [][]int, n int, k int) int {
+	graph := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		graph[i] = make([]int, n+1)
+		for j := 0; j <= n; j++ {
+			graph[i][j] = -1
+		}
+	}
+	for _, time := range times {
+		graph[time[0]][time[1]] = time[2]
+	}
+	netTimes := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		netTimes[i] = math.MaxInt
+	}
+	netTimes[k] = 0
+	// 广度优先遍历
+	queue := list.New()
+	queue.PushBack(k)
+	visited := make([]bool, n+1)
+	visited[k] = true
+	for queue.Len() > 0 {
+		front := queue.Front()
+		queue.Remove(front)
+		start := front.Value.(int)
+		visited[start] = false
+		for i := 1; i <= n; i++ {
+			if graph[start][i] == -1 {
+				continue
+			}
+			if netTimes[i] > netTimes[start]+graph[start][i] {
+				netTimes[i] = netTimes[start] + graph[start][i]
+				if !visited[i] {
+					queue.PushBack(i)
+					visited[i] = true
+				}
+			}
+		}
+	}
+	result := 0
+	for i := 1; i <= n; i++ {
+		t := netTimes[i]
+		if t == math.MaxInt {
+			return -1
+		}
+		result = max(t, result)
+	}
+	return result
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
