19a722f7e78536c61d3277f5c5e3a8f44903ce3f
qingfengsa <15800012123@163.com>
2022-08-06 09:39:28 +0800

    LeetCode 2022-08-06 1408. 数组中的字符串匹配

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -4440,3 +4440,45 @@ func orderlyQueue(s string, k int) string {
 	})
 	return string(t)
 }
+
+// 1408. 数组中的字符串匹配
+// 给你一个字符串数组 words ，数组中的每个字符串都可以看作是一个单词。请你按 任意 顺序返回 words 中是其他单词的子字符串的所有单词。
+//
+// 如果你可以删除 words[j] 最左侧和/或最右侧的若干字符得到 word[i] ，那么字符串 words[i] 就是 words[j] 的一个子字符串。
+//
+// 示例 1：
+// 输入：words = ["mass","as","hero","superhero"]
+// 输出：["as","hero"]
+// 解释："as" 是 "mass" 的子字符串，"hero" 是 "superhero" 的子字符串。
+// ["hero","as"] 也是有效的答案。
+//
+// 示例 2：
+// 输入：words = ["leetcode","et","code"]
+// 输出：["et","code"]
+// 解释："et" 和 "code" 都是 "leetcode" 的子字符串。
+//
+// 示例 3：
+// 输入：words = ["blue","green","bu"]
+// 输出：[]
+//
+// 提示：
+// 1 <= words.length <= 100
+// 1 <= words[i].length <= 30
+// words[i] 仅包含小写英文字母。
+// 题目数据 保证 每个 words[i] 都是独一无二的。
+func stringMatching(words []string) []string {
+	sort.Slice(words, func(i, j int) bool {
+		return len(words[i]) < len(words[j])
+	})
+	n := len(words)
+	result := make([]string, 0)
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if strings.Contains(words[j], words[i]) {
+				result = append(result, words[i])
+				break
+			}
+		}
+	}
+	return result
+}
