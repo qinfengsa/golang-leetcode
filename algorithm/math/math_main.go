6f216044f2467a0eeaf0cbd18e10f185fbe2d17a
wangheng <15800012123@163.com>
2022-08-08 13:12:22 +0800

    LeetCode 2022-08-08 752. 打开转盘锁 754. 到达终点数字 761. 特殊的二进制序列

--- algorithm/math/math_main.go
+++ algorithm/math/math_main.go
@@ -3528,3 +3528,51 @@ func splitExpression(expression string) []string {
 	result = append(result, expression[start:])
 	return result
 }
+
+// 754. 到达终点数字
+// 在一根无限长的数轴上，你站在0的位置。终点在target的位置。
+//
+// 你可以做一些数量的移动 numMoves :
+//
+// 每次你可以选择向左或向右移动。
+// 第 i 次移动（从  i == 1 开始，到 i == numMoves ），在选择的方向上走 i 步。
+// 给定整数 target ，返回 到达目标所需的 最小 移动次数(即最小 numMoves ) 。
+//
+// 示例 1:
+// 输入: target = 2
+// 输出: 3
+// 解释:
+// 第一次移动，从 0 到 1 。
+// 第二次移动，从 1 到 -1 。
+// 第三次移动，从 -1 到 2 。
+//
+// 示例 2:
+// 输入: target = 3
+// 输出: 2
+// 解释:
+// 第一次移动，从 0 到 1 。
+// 第二次移动，从 1 到 3 。
+//
+// 提示:
+// -109 <= target <= 109
+// target != 0
+func reachNumber(target int) int {
+	target = abs(target)
+	k := 0
+	for target > 0 {
+		k++
+		target -= k
+	}
+	// sum(1 ~ k) > target
+	// 让部分元素变成负数
+	// delta 是 余数
+	// 1. 余数是偶数 可以 通过 把 (delta/2)个数字变成负数  -1 + 2 - 3 + 4 (构造 2个 1)
+
+	if target&1 == 0 {
+		return k
+	}
+
+	// 2. 余数是奇数
+	return k + 1 + k%2
+
+}
