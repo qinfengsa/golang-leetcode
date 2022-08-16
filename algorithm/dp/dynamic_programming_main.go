0e6e180345073678bbcf437e9d5c4142ab4f8070
wangheng <15800012123@163.com>
2022-08-16 12:17:49 +0800

    LeetCode 2022-08-16 763. 划分字母区间 764. 最大加号标志 1656. 设计有序流

--- algorithm/dp/dynamic_programming_main.go
+++ algorithm/dp/dynamic_programming_main.go
@@ -3510,3 +3510,94 @@ func cherryPickup(grid [][]int) int {
 
 	return max(0, dp[0][0])
 }
+
+// 764. 最大加号标志
+// 在一个 n x n 的矩阵 grid 中，除了在数组 mines 中给出的元素为 0，其他每个元素都为 1。mines[i] = [xi, yi]表示 grid[xi][yi] == 0
+//
+// 返回  grid 中包含 1 的最大的 轴对齐 加号标志的阶数 。如果未找到加号标志，则返回 0 。
+//
+// 一个 k 阶由 1 组成的 “轴对称”加号标志 具有中心网格 grid[r][c] == 1 ，以及4个从中心向上、向下、向左、向右延伸，长度为 k-1，由 1 组成的臂。注意，只有加号标志的所有网格要求为 1 ，别的网格可能为 0 也可能为 1 。
+//
+// 示例 1：
+// 输入: n = 5, mines = [[4, 2]]
+// 输出: 2
+// 解释: 在上面的网格中，最大加号标志的阶只能是2。一个标志已在图中标出。
+//
+// 示例 2：
+// 输入: n = 1, mines = [[0, 0]]
+// 输出: 0
+// 解释: 没有加号标志，返回 0 。
+//
+// 提示：
+// 1 <= n <= 500
+// 1 <= mines.length <= 5000
+// 0 <= xi, yi < n
+// 每一对 (xi, yi) 都 不重复
+func orderOfLargestPlusSign(n int, mines [][]int) int {
+	if n == 0 {
+		return 0
+	}
+	dp, matrix := make([][]int, n), make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+		matrix[i] = make([]int, n)
+		for j := 0; j < n; j++ {
+			matrix[i][j] = 1
+		}
+	}
+
+	for _, mine := range mines {
+		row, col := mine[0], mine[1]
+		matrix[row][col] = 0
+	}
+	count := 0
+	// 左右
+	for i := 0; i < n; i++ {
+		count = 0
+		// 左臂
+		for j := 0; j < n; j++ {
+			if matrix[i][j] == 1 {
+				count++
+			} else {
+				count = 0
+			}
+			dp[i][j] = count
+		}
+		// 右臂
+		count = 0
+		for j := n - 1; j >= 0; j-- {
+			if matrix[i][j] == 1 {
+				count++
+			} else {
+				count = 0
+			}
+			dp[i][j] = min(dp[i][j], count)
+		}
+	}
+	result := 0
+	// 上下
+	for j := 0; j < n; j++ {
+		count = 0
+		// 上臂
+		for i := 0; i < n; i++ {
+			if matrix[i][j] == 1 {
+				count++
+			} else {
+				count = 0
+			}
+			dp[i][j] = min(dp[i][j], count)
+		}
+		// 下臂
+		count = 0
+		for i := n - 1; i >= 0; i-- {
+			if matrix[i][j] == 1 {
+				count++
+			} else {
+				count = 0
+			}
+			dp[i][j] = min(dp[i][j], count)
+			result = max(result, dp[i][j])
+		}
+	}
+	return result
+}
