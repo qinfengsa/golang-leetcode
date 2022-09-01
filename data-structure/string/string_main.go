dfeddc01a9cf52a8c95c05889f2df5f41844e7c7
wangheng <15800012123@163.com>
2022-09-01 10:07:11 +0800

    LeetCode 2022-09-01 777. 在LR字符串中交换相邻字符 1475. 商品折扣后的最终价格

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -4737,3 +4737,42 @@ func reorganizeString(s string) string {
 
 	return string(result)
 }
+
+// 777. 在LR字符串中交换相邻字符
+// 在一个由 'L' , 'R' 和 'X' 三个字符组成的字符串（例如"RXXLRXRXL"）中进行移动操作。
+// 一次移动操作指用一个"LX"替换一个"XL"，或者用一个"XR"替换一个"RX"。现给定起始字符串start和结束字符串end，请编写代码，当且仅当存在一系列移动操作使得start可以转换成end时， 返回True。
+//
+// 示例 :
+// 输入: start = "RXXLRXRXL", end = "XRLXXRRLX"
+// 输出: True
+// 解释:
+// 我们可以通过以下几步将start转换成end:
+// RXXLRXRXL ->
+// XRXLRXRXL ->
+// XRLXRXRXL ->
+// XRLXXRRXL ->
+// XRLXXRRLX
+//
+// 提示：
+// 1 <= len(start) = len(end) <= 10000。
+// start和end中的字符串仅限于'L', 'R'和'X'。
+func canTransform(start string, end string) bool {
+	// 有 X 才能移动 移除X后 两个字符串相同
+	if strings.ReplaceAll(start, "X", "") != strings.ReplaceAll(end, "X", "") {
+		return false
+	}
+	j := 0
+	for i, c := range start {
+		if c != 'X' {
+			// c = L 或者 R
+			for end[j] == 'X' {
+				j++
+			}
+			if i != j && c == 'L' != (i > j) {
+				return false
+			}
+			j++
+		}
+	}
+	return true
+}
