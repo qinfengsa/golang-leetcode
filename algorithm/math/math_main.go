a957cc64668c07023a9186b98afc61e5ca54b014
wangheng <15800012123@163.com>
2022-09-09 10:38:23 +0800

    LeetCode 2022-09-09 780. 到达终点 1598. 文件夹操作日志搜集器

--- algorithm/math/math_main.go
+++ algorithm/math/math_main.go
@@ -3658,3 +3658,46 @@ func kthGrammar(n int, k int) int {
 		return evenNum
 	}
 }
+
+// 780. 到达终点
+// 给定四个整数 sx , sy ，tx 和 ty，如果通过一系列的转换可以从起点 (sx, sy) 到达终点 (tx, ty)，则返回 true，否则返回 false。
+//
+// 从点 (x, y) 可以转换到 (x, x+y)  或者 (x+y, y)。
+//
+// 示例 1:
+// 输入: sx = 1, sy = 1, tx = 3, ty = 5
+// 输出: true
+// 解释:
+// 可以通过以下一系列转换从起点转换到终点：
+// (1, 1) -> (1, 2)
+// (1, 2) -> (3, 2)
+// (3, 2) -> (3, 5)
+//
+// 示例 2:
+// 输入: sx = 1, sy = 1, tx = 2, ty = 2
+// 输出: false
+//
+// 示例 3:
+// 输入: sx = 1, sy = 1, tx = 1, ty = 1
+// 输出: true
+//
+// 提示:
+// 1 <= sx, sy, tx, ty <= 109
+func reachingPoints(sx int, sy int, tx int, ty int) bool {
+	if sx > tx || sy > ty {
+		return false
+	}
+	if tx > ty { // x 更大
+		if sy == ty {
+			return (tx-sx)%sy == 0
+		}
+
+		return reachingPoints(sx, sy, tx%ty, ty)
+	} else if tx < ty { // y 更大
+		if sx == tx {
+			return (ty-sy)%sx == 0
+		}
+		return reachingPoints(sx, sy, tx, ty%tx)
+	}
+	return sx == tx && sy == ty
+}
