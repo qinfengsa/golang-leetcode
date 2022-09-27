51e7823b179cf971650bd4dea4e9def8601cc2da
wangheng <15800012123@163.com>
2022-09-27 09:24:32 +0800

    LeetCode 2022-09-27 面试题 01.02. 判定是否互为字符重排

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -4997,3 +4997,34 @@ func maxLengthBetweenEqualCharacters(s string) int {
 	}
 	return result
 }
+
+// 面试题 01.02. 判定是否互为字符重排
+// 给定两个字符串 s1 和 s2，请编写一个程序，确定其中一个字符串的字符重新排列后，能否变成另一个字符串。
+//
+// 示例 1：
+// 输入: s1 = "abc", s2 = "bca"
+// 输出: true
+//
+// 示例 2：
+// 输入: s1 = "abc", s2 = "bad"
+// 输出: false
+//
+// 说明：
+// 0 <= len(s1) <= 100
+// 0 <= len(s2) <= 100
+func checkPermutation(s1 string, s2 string) bool {
+	letters := make([]int, 26)
+	for _, c := range s1 {
+		letters[c-'a']++
+	}
+	for _, c := range s2 {
+		letters[c-'a']--
+	}
+	for i := 0; i < 26; i++ {
+		if letters[i] != 0 {
+			return false
+		}
+	}
+
+	return true
+}
