1f42ac2e1139a09256a0d0866546f6c3b6941bbf
wangheng <15800012123@163.com>
2022-09-28 09:26:22 +0800

    LeetCode 2022-09-28 面试题 17.09. 第 k 个数

--- data-structure/arrays/arrays_main.go
+++ data-structure/arrays/arrays_main.go
@@ -5821,3 +5821,29 @@ func decrypt(code []int, k int) []int {
 	}
 	return result
 }
+
+// 面试题 17.09. 第 k 个数
+// 有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+//
+// 示例 1:
+// 输入: k = 5
+// 输出: 9
+func getKthMagicNumber(k int) int {
+	idx1, idx2, idx3 := 0, 0, 0
+	nums := make([]int, k)
+	nums[0] = 1
+	for i := 1; i < k; i++ {
+		nums[i] = min(nums[idx1]*3, nums[idx2]*5, nums[idx3]*7)
+		if nums[i] == nums[idx1]*3 {
+			idx1++
+		}
+		if nums[i] == nums[idx2]*5 {
+			idx2++
+		}
+		if nums[i] == nums[idx3]*7 {
+			idx3++
+		}
+	}
+
+	return nums[k-1]
+}
