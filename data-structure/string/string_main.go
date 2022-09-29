91b1cb0084de348246f8e274db80df395fdc6e9a
wangheng <15800012123@163.com>
2022-09-29 08:28:38 +0800

    LeetCode 2022-09-29 面试题 01.09. 字符串轮转

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -5028,3 +5028,27 @@ func checkPermutation(s1 string, s2 string) bool {
 
 	return true
 }
+
+// 面试题 01.09. 字符串轮转
+// 字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+//
+// 示例1:
+// 输入：s1 = "waterbottle", s2 = "erbottlewat"
+// 输出：True
+//
+// 示例2:
+// 输入：s1 = "aa", s2 = "aba"
+// 输出：False
+//
+// 提示：
+// 字符串长度在[0, 100000]范围内。
+// 说明:
+// 你能只调用一次检查子串的方法吗？
+func isFlipedString(s1 string, s2 string) bool {
+	m, n := len(s1), len(s2)
+	if m != n {
+		return false
+	}
+
+	return strings.Contains(s1+s1, s2)
+}
