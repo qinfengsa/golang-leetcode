8a4b4bd44dac4c4458779c08247db9728260fd1a
wangheng <15800012123@163.com>
2022-10-09 10:18:22 +0800

    LeetCode 2022-10-09 856. 括号的分数

--- data-structure/stack/stack_main.go
+++ data-structure/stack/stack_main.go
@@ -1143,3 +1143,50 @@ func finalPrices(prices []int) []int {
 
 	return result
 }
+
+// 856. 括号的分数
+// 给定一个平衡括号字符串 S，按下述规则计算该字符串的分数：
+//
+// () 得 1 分。
+// AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。
+// (A) 得 2 * A 分，其中 A 是平衡括号字符串。
+//
+// 示例 1：
+// 输入： "()"
+// 输出： 1
+//
+// 示例 2：
+// 输入： "(())"
+// 输出： 2
+//
+// 示例 3：
+// 输入： "()()"
+// 输出： 2
+//
+// 示例 4：
+// 输入： "(()(()))"
+// 输出： 6
+//
+// 提示：
+// S 是平衡括号字符串，且只含有 ( 和 ) 。
+// 2 <= S.length <= 50
+func scoreOfParentheses(s string) int {
+	stack := list.New()
+	stack.PushBack(0)
+	for _, c := range s {
+		if c == '(' {
+			stack.PushBack(0)
+		} else {
+			back := stack.Back()
+			stack.Remove(back)
+			v := back.Value.(int)
+			back = stack.Back()
+			stack.Remove(back)
+			w := back.Value.(int)
+
+			stack.PushBack(w + max(2*v, 1))
+		}
+	}
+	back := stack.Back()
+	return back.Value.(int)
+}
