385de86164afa12720a22f797a869421969eda88
wangheng <15800012123@163.com>
2022-10-10 10:05:02 +0800

    LeetCode 2022-10-10 801. 使序列递增的最小交换次数

--- algorithm/dp/dynamic_programming_main.go
+++ algorithm/dp/dynamic_programming_main.go
@@ -3601,3 +3601,52 @@ func orderOfLargestPlusSign(n int, mines [][]int) int {
 	}
 	return result
 }
+
+// 801. 使序列递增的最小交换次数
+// 我们有两个长度相等且不为空的整型数组 nums1 和 nums2 。在一次操作中，我们可以交换 nums1[i] 和 nums2[i]的元素。
+//
+// 例如，如果 nums1 = [1,2,3,8] ， nums2 =[5,6,7,4] ，你可以交换 i = 3 处的元素，得到 nums1 =[1,2,3,4] 和 nums2 =[5,6,7,8] 。
+// 返回 使 nums1 和 nums2 严格递增 所需操作的最小次数 。
+//
+// 数组 arr 严格递增 且  arr[0] < arr[1] < arr[2] < ... < arr[arr.length - 1] 。
+//
+// 注意：
+// 用例保证可以实现操作。
+//
+// 示例 1:
+// 输入: nums1 = [1,3,5,4], nums2 = [1,2,3,7]
+// 输出: 1
+// 解释:
+// 交换 A[3] 和 B[3] 后，两个数组如下:
+// A = [1, 3, 5, 7] ， B = [1, 2, 3, 4]
+// 两个数组均为严格递增的。
+//
+// 示例 2:
+// 输入: nums1 = [0,3,5,8,9], nums2 = [2,1,4,6,9]
+// 输出: 1
+//
+// 提示:
+// 2 <= nums1.length <= 105
+// nums2.length == nums1.length
+// 0 <= nums1[i], nums2[i] <= 2 * 105
+func minSwap(nums1 []int, nums2 []int) int {
+	n := len(nums1)
+	swap, noswap := 1, 0
+	for i := 1; i < n; i++ {
+		tmpSwap, tmpNoswap := math.MaxInt32, math.MaxInt32
+		if nums1[i-1] < nums1[i] && nums2[i-1] < nums2[i] {
+			// 不进行交换
+			tmpNoswap = min(tmpNoswap, noswap)
+			tmpSwap = min(tmpSwap, swap+1)
+
+		}
+		if nums1[i-1] < nums2[i] && nums2[i-1] < nums1[i] {
+			// 进行交换
+			tmpNoswap = min(tmpNoswap, swap)
+			tmpSwap = min(tmpSwap, noswap+1)
+		}
+		swap = tmpSwap
+		noswap = tmpNoswap
+	}
+	return min(swap, noswap)
+}
