a58806d5e2d5a6f41ddd305c4b33a7d693c780ee
qingfengsa <15800012123@163.com>
2022-10-16 09:01:53 +0800

    LeetCode 2022-10-16 886. 可能的二分法

--- data-structure/arrays/arrays_main.go
+++ data-structure/arrays/arrays_main.go
@@ -5937,3 +5937,74 @@ func advantageCount(nums1 []int, nums2 []int) []int {
 
 	return result
 }
+
+// 886. 可能的二分法
+// 给定一组 n 人（编号为 1, 2, ..., n）， 我们想把每个人分进任意大小的两组。每个人都可能不喜欢其他人，那么他们不应该属于同一组。
+//
+// 给定整数 n 和数组 dislikes ，其中 dislikes[i] = [ai, bi] ，表示不允许将编号为 ai 和  bi的人归入同一组。当可以用这种方法将所有人分进两组时，返回 true；否则返回 false。
+//
+// 示例 1：
+// 输入：n = 4, dislikes = [[1,2],[1,3],[2,4]]
+// 输出：true
+// 解释：group1 [1,4], group2 [2,3]
+//
+// 示例 2：
+// 输入：n = 3, dislikes = [[1,2],[1,3],[2,3]]
+// 输出：false
+//
+// 示例 3：
+// 输入：n = 5, dislikes = [[1,2],[2,3],[3,4],[4,5],[1,5]]
+// 输出：false
+//
+// 提示：
+// 1 <= n <= 2000
+// 0 <= dislikes.length <= 104
+// dislikes[i].length == 2
+// 1 <= dislikes[i][j] <= n
+// ai < bi
+// dislikes 中每一组都 不同
+func possibleBipartition(n int, dislikes [][]int) bool {
+	// 染色
+	uncolor, white, black := 0, 1, 2
+	relations := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		relations[i] = make([]int, 0)
+	}
+	for _, dislike := range dislikes {
+		a, b := dislike[0], dislike[1]
+		relations[a] = append(relations[a], b)
+		relations[b] = append(relations[b], a)
+	}
+	colors := make([]int, n+1)
+
+	var dfs func(num int) bool
+
+	dfs = func(num int) bool {
+		if colors[num] == uncolor {
+			colors[num] = white
+		}
+		relation := relations[num]
+		dislike := white
+		if colors[num] == white {
+			dislike = black
+		}
+		for _, p := range relation {
+			if colors[p] == uncolor {
+				colors[p] = dislike
+				if !dfs(p) {
+					return false
+				}
+			} else if colors[p] != dislike {
+				return false
+			}
+		}
+		return true
+	}
+
+	for i := 1; i <= n; i++ {
+		if !dfs(i) {
+			return false
+		}
+	}
+	return true
+}
