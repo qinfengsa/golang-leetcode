b9e03c603f0a3e7d58603b9393638efb9ba101dc
wangheng <15800012123@163.com>
2022-10-28 14:18:35 +0800

    LeetCode 2022-10-28 907. 子数组的最小值之和

--- data-structure/stack/stack_main.go
+++ data-structure/stack/stack_main.go
@@ -1243,3 +1243,56 @@ func buildArray(target []int, n int) []string {
 
 	return result
 }
+
+// 907. 子数组的最小值之和
+// 给定一个整数数组 arr，找到 min(b) 的总和，其中 b 的范围为 arr 的每个（连续）子数组。
+//
+// 由于答案可能很大，因此 返回答案模 10^9 + 7 。
+//
+// 示例 1：
+// 输入：arr = [3,1,2,4]
+// 输出：17
+// 解释：
+// 子数组为 [3]，[1]，[2]，[4]，[3,1]，[1,2]，[2,4]，[3,1,2]，[1,2,4]，[3,1,2,4]。
+// 最小值为 3，1，2，4，1，1，2，1，1，1，和为 17。
+//
+// 示例 2：
+// 输入：arr = [11,81,94,43,3]
+// 输出：444
+//
+// 提示：
+// 1 <= arr.length <= 3 * 104
+// 1 <= arr[i] <= 3 * 104
+func sumSubarrayMins(arr []int) int {
+	MOD := 1_000_000_007
+	stack := list.New()
+	result := 0
+	tmp := 0
+	for _, num := range arr {
+		count := 1
+		// 奇数当前num 在多少个子数组最小
+		back := stack.Back()
+		for stack.Len() > 0 {
+			node := back.Value.(RepInt)
+			// 当前num是最大的
+			if node.val < num {
+				break
+			}
+			count += node.count
+			tmp -= node.val * node.count
+			stack.Remove(back)
+			back = stack.Back()
+		}
+		stack.PushBack(RepInt{num, count})
+		// 子数组
+		tmp += num * count
+		result += tmp
+		result %= MOD
+	}
+	return result
+}
+
+type RepInt struct {
+	val   int
+	count int
+}
