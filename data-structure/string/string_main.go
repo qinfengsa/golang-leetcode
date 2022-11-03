289c3b1952a821cfd59f6a0e1412e12395b8b93e
wangheng <15800012123@163.com>
2022-11-03 10:12:11 +0800

    LeetCode 2022-11-03 1668. 最大重复子字符串

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -5422,3 +5422,59 @@ func arrayStringsAreEqual(word1 []string, word2 []string) bool {
 	}
 	return builder1.String() == builder2.String()
 }
+
+// 1668. 最大重复子字符串
+// 给你一个字符串 sequence ，如果字符串 word 连续重复 k 次形成的字符串是 sequence 的一个子字符串，那么单词 word 的 重复值为 k 。单词 word 的 最大重复值 是单词 word 在 sequence 中最大的重复值。如果 word 不是 sequence 的子串，那么重复值 k 为 0 。
+//
+// 给你一个字符串 sequence 和 word ，请你返回 最大重复值 k 。
+//
+// 示例 1：
+// 输入：sequence = "ababc", word = "ab"
+// 输出：2
+// 解释："abab" 是 "ababc" 的子字符串。
+//
+// 示例 2：
+// 输入：sequence = "ababc", word = "ba"
+// 输出：1
+// 解释："ba" 是 "ababc" 的子字符串，但 "baba" 不是 "ababc" 的子字符串。
+//
+// 示例 3：
+// 输入：sequence = "ababc", word = "ac"
+// 输出：0
+// 解释："ac" 不是 "ababc" 的子字符串。
+//
+// 提示：
+// 1 <= sequence.length <= 100
+// 1 <= word.length <= 100
+// sequence 和 word 都只包含小写英文字母。
+func maxRepeating(sequence string, word string) int {
+	m, n := len(sequence), len(word)
+	if m < n {
+		return 0
+	}
+	getRepeat := func(start int) int {
+		count := 0
+	out:
+		for i := start; i <= m-n; i += n {
+			for j := 0; j < n; j++ {
+				if sequence[i+j] != word[j] {
+					break out
+				}
+			}
+			count++
+		}
+
+		return count
+	}
+
+	result := 0
+	for i := 0; i < m; i++ {
+		if sequence[i] != word[0] {
+			continue
+		}
+		count := getRepeat(i)
+		result = max(result, count)
+		//i += count * n
+	}
+	return result
+}
