528d806f3b300b582cb2ede3e114430e884a5858
qingfengsa <15800012123@163.com>
2022-12-03 09:30:50 +0800

    LeetCode 2022-12-03 1796. 字符串中第二大的数字

--- data-structure/string/string_main.go
+++ data-structure/string/string_main.go
@@ -5678,6 +5678,40 @@ func halvesAreAlike(s string) bool {
 	return count == 0
 }
 
+// 1796. 字符串中第二大的数字
+// 给你一个混合字符串 s ，请你返回 s 中 第二大 的数字，如果不存在第二大的数字，请你返回 -1 。
+// 混合字符串 由小写英文字母和数字组成。
+//
+// 示例 1：
+// 输入：s = "dfa12321afd"
+// 输出：2
+// 解释：出现在 s 中的数字包括 [1, 2, 3] 。第二大的数字是 2 。
+//
+// 示例 2：
+// 输入：s = "abc1111"
+// 输出：-1
+// 解释：出现在 s 中的数字只包含 [1] 。没有第二大的数字。
+//
+// 提示：
+// 1 <= s.length <= 500
+// s 只包含小写英文字母和（或）数字。
+func secondHighest(s string) int {
+	maxVal, second := -1, -1
+	for _, c := range s {
+		if c >= '0' && c <= '9' {
+			num := int(c - '0')
+			if num > maxVal {
+				second = maxVal
+				maxVal = num
+			} else if num < maxVal && num > second {
+				second = num
+			}
+		}
+	}
+
+	return second
+}
+
 // 809. 情感丰富的文字
 // 有时候人们会用重复写一些字母来表示额外的感受，比如 "hello" -> "heeellooo", "hi" -> "hiii"。我们将相邻字母都相同的一串字符定义为相同字母组，例如："h", "eee", "ll", "ooo"。
 //
