8236072b89053d20f814d3f6510f7059840e3567
wangheng <15800012123@163.com>
2022-12-07 10:37:21 +0800

    LeetCode 2022-12-07 1775. 通过最少操作次数使数组的和相等

--- data-structure/arrays/arrays_main.go
+++ data-structure/arrays/arrays_main.go
@@ -6383,3 +6383,104 @@ func minOperations(boxes string) []int {
 
 	return result
 }
+
+// 1775. 通过最少操作次数使数组的和相等
+// 给你两个长度可能不等的整数数组 nums1 和 nums2 。两个数组中的所有值都在 1 到 6 之间（包含 1 和 6）。
+//
+// 每次操作中，你可以选择 任意 数组中的任意一个整数，将它变成 1 到 6 之间 任意 的值（包含 1 和 6）。
+//
+// 请你返回使 nums1 中所有数的和与 nums2 中所有数的和相等的最少操作次数。如果无法使两个数组的和相等，请返回 -1 。
+//
+// 示例 1：
+// 输入：nums1 = [1,2,3,4,5,6], nums2 = [1,1,2,2,2,2]
+// 输出：3
+// 解释：你可以通过 3 次操作使 nums1 中所有数的和与 nums2 中所有数的和相等。以下数组下标都从 0 开始。
+// - 将 nums2[0] 变为 6 。 nums1 = [1,2,3,4,5,6], nums2 = [6,1,2,2,2,2] 。
+// - 将 nums1[5] 变为 1 。 nums1 = [1,2,3,4,5,1], nums2 = [6,1,2,2,2,2] 。
+// - 将 nums1[2] 变为 2 。 nums1 = [1,2,2,4,5,1], nums2 = [6,1,2,2,2,2] 。
+//
+// 示例 2：
+// 输入：nums1 = [1,1,1,1,1,1,1], nums2 = [6]
+// 输出：-1
+// 解释：没有办法减少 nums1 的和或者增加 nums2 的和使二者相等。
+//
+// 示例 3：
+// 输入：nums1 = [6,6], nums2 = [1]
+// 输出：3
+// 解释：你可以通过 3 次操作使 nums1 中所有数的和与 nums2 中所有数的和相等。以下数组下标都从 0 开始。
+// - 将 nums1[0] 变为 2 。 nums1 = [2,6], nums2 = [1] 。
+// - 将 nums1[1] 变为 2 。 nums1 = [2,2], nums2 = [1] 。
+// - 将 nums2[0] 变为 4 。 nums1 = [2,2], nums2 = [4] 。
+//
+// 提示：
+// 1 <= nums1.length, nums2.length <= 105
+// 1 <= nums1[i], nums2[i] <= 6
+func minOperationsII(nums1 []int, nums2 []int) int {
+	if len(nums1) < len(nums2) {
+		nums1, nums2 = nums2, nums1
+	}
+	m, n := len(nums1), len(nums2)
+	if m > n*6 {
+		return -1
+	}
+	count1, count2 := 0, 0
+	if m == n*6 {
+		for _, num := range nums1 {
+			if num != 1 {
+				count1++
+			}
+		}
+		for _, num := range nums2 {
+			if num != 6 {
+				count2++
+			}
+		}
+		return count1 + count2
+	}
+	sum1, sum2 := 0, 0
+	for _, num := range nums1 {
+		sum1 += num
+	}
+	for _, num := range nums2 {
+		sum2 += num
+	}
+	if sum1 == sum2 {
+		return 0
+	}
+	arr1, arr2 := make([]int, 6), make([]int, 6)
+	for _, num := range nums1 {
+		arr1[num-1]++
+	}
+	for _, num := range nums2 {
+		arr2[num-1]++
+	}
+	if sum1 < sum2 {
+		arr1, arr2 = arr2, arr1
+	}
+	result := 0
+	// 把 sum 大的arr1大元素变成1, 或者把 sum 小的arr2小元素变成6
+	diffNum := abs(sum1 - sum2)
+	for i := 0; i < 5; i++ {
+		if diffNum == 0 {
+			break
+		}
+		num, count := 5-i, arr2[i]+arr1[5-i]
+		if count*num >= diffNum {
+
+			count = diffNum / num
+			if diffNum%num != 0 {
+				count++
+			}
+			result += count
+			diffNum = 0
+			break
+		}
+		diffNum -= count * num
+		result += count
+	}
+	if diffNum == 0 {
+		return result
+	}
+
+	return -1
+}
