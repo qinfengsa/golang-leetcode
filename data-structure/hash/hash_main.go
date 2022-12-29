fe414c6a37c50ed71f7cd19f9b93a012a8814693
wangheng <15800012123@163.com>
2022-12-29 18:14:54 +0800

    LeetCode 2022-12-29 2032. 至少在两个数组中出现的值

--- data-structure/hash/hash_main.go
+++ data-structure/hash/hash_main.go
@@ -1856,3 +1856,50 @@ type WordNode struct {
 	word  string
 	index int
 }
+
+// 2032. 至少在两个数组中出现的值
+//
+// 给你三个整数数组 nums1、nums2 和 nums3 ，请你构造并返回一个 元素各不相同的 数组，且由 至少 在 两个 数组中出现的所有值组成。数组中的元素可以按 任意 顺序排列。
+//
+// 示例 1：
+// 输入：nums1 = [1,1,3,2], nums2 = [2,3], nums3 = [3]
+// 输出：[3,2]
+// 解释：至少在两个数组中出现的所有值为：
+// - 3 ，在全部三个数组中都出现过。
+// - 2 ，在数组 nums1 和 nums2 中出现过。
+//
+// 示例 2：
+// 输入：nums1 = [3,1], nums2 = [2,3], nums3 = [1,2]
+// 输出：[2,3,1]
+// 解释：至少在两个数组中出现的所有值为：
+// - 2 ，在数组 nums2 和 nums3 中出现过。
+// - 3 ，在数组 nums1 和 nums2 中出现过。
+// - 1 ，在数组 nums1 和 nums3 中出现过。
+//
+// 示例 3：
+// 输入：nums1 = [1,2,2], nums2 = [4,3,3], nums3 = [5]
+// 输出：[]
+// 解释：不存在至少在两个数组中出现的值。
+//
+// 提示：
+// 1 <= nums1.length, nums2.length, nums3.length <= 100
+// 1 <= nums1[i], nums2[j], nums3[k] <= 100
+func twoOutOfThree(nums1 []int, nums2 []int, nums3 []int) []int {
+	hashNums := make([]int, 101)
+	for _, num := range nums1 {
+		hashNums[num] |= 1
+	}
+	for _, num := range nums2 {
+		hashNums[num] |= 2
+	}
+	for _, num := range nums3 {
+		hashNums[num] |= 4
+	}
+	result := make([]int, 0)
+	for i := 1; i <= 100; i++ {
+		if hashNums[i] == 3 || hashNums[i] == 5 || hashNums[i] == 6 || hashNums[i] == 7 {
+			result = append(result, i)
+		}
+	}
+	return result
+}
