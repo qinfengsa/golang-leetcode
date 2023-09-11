49f434c4603d99e814df60aee50ad234dc90535f
wangheng <15800012123@163.com>
2023-09-11 10:10:38 +0800

    LeetCode 2023-09-11 812. 最大三角形面积

--- algorithm/math/math_main.go
+++ algorithm/math/math_main.go
@@ -4077,3 +4077,49 @@ func minNumber(nums1 []int, nums2 []int) int {
 	}
 	return min(x*10+y, y*10+x)
 }
+
+// 812. 最大三角形面积
+// 给你一个由 X-Y 平面上的点组成的数组 points ，其中 points[i] = [xi, yi] 。从其中取任意三个不同的点组成三角形，返回能组成的最大三角形的面积。与真实值误差在 10-5 内的答案将会视为正确答案。
+//
+// 示例 1：
+// 输入：points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+// 输出：2.00000
+// 解释：输入中的 5 个点如上图所示，红色的三角形面积最大。
+//
+// 示例 2：
+// 输入：points = [[1,0],[0,0],[0,1]]
+// 输出：0.50000
+//
+// 提示：
+// 3 <= points.length <= 50
+// -50 <= xi, yi <= 50
+// 给出的所有点 互不相同
+func largestTriangleArea(points [][]int) float64 {
+
+	calcArea := func(point1, point2, point3 []int) float64 {
+		x1, y1 := point1[0], point1[1]
+		x2, y2 := point2[0], point2[1]
+		x3, y3 := point3[0], point3[1]
+
+		var result = 0.5 * float64(x1*y3+x2*y1+x3*y2-x1*y2-x2*y3-x3*y1)
+		// sum = 1/2 (x1*y3 + x2*y1 + x3*y2 - x1*y2 - x2*y3 - x3*y1)
+
+		if result < 0 {
+			result = -result
+		}
+		return result
+	}
+	var result = 0.0
+	n := len(points)
+	for i := 0; i < n-2; i++ {
+		for j := 0; j < n-1; j++ {
+			for k := 0; k < n; k++ {
+				area := calcArea(points[i], points[j], points[k])
+				if area > result {
+					result = area
+				}
+			}
+		}
+	}
+	return result
+}
