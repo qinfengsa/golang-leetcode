d3cebb6e2e505443768935e8721b69084c96908f
wangheng <15800012123@163.com>
2023-10-09 14:18:24 +0800

    LeetCode 2023-10-09 2578. 最小和分割

--- algorithm/greedy/greedy_main.go
+++ algorithm/greedy/greedy_main.go
@@ -1001,5 +1001,46 @@ func distMoney(money int, children int) int {
 		count--
 	}
 	return count
+}
 
+// 2578. 最小和分割
+// 给你一个正整数 num ，请你将它分割成两个非负整数 num1 和 num2 ，满足：
+// num1 和 num2 直接连起来，得到 num 各数位的一个排列。
+// 换句话说，num1 和 num2 中所有数字出现的次数之和等于 num 中所有数字出现的次数。
+// num1 和 num2 可以包含前导 0 。
+// 请你返回 num1 和 num2 可以得到的和的 最小 值。
+//
+// 注意：
+// num 保证没有前导 0 。
+// num1 和 num2 中数位顺序可以与 num 中数位顺序不同。
+//
+// 示例 1：
+// 输入：num = 4325
+// 输出：59
+// 解释：我们可以将 4325 分割成 num1 = 24 和 num2 = 35 ，和为 59 ，59 是最小和。
+//
+// 示例 2：
+// 输入：num = 687
+// 输出：75
+// 解释：我们可以将 687 分割成 num1 = 68 和 num2 = 7 ，和为最优值 75 。
+//
+// 提示：
+// 10 <= num <= 109
+func splitNum(num int) int {
+	nums := make([]int, 0)
+	for num > 0 {
+		nums = append(nums, num%10)
+		num = num / 10
+	}
+	sort.Ints(nums)
+	n := len(nums)
+	num1, num2 := 0, 0
+	for i := 0; i < n; i += 2 {
+		num1 = num1*10 + nums[i]
+	}
+	for i := 1; i < n; i += 2 {
+		num2 = num2*10 + nums[i]
+	}
+
+	return num1 + num2
 }
