93206639f763c46b6ebfa59efcaa7209fd34715d
qingfengsa <15800012123@163.com>
2023-10-14 11:47:37 +0800

    LeetCode 2023-10-14 896. 单调数列

--- data-structure/arrays/arrays_main.go
+++ data-structure/arrays/arrays_main.go
@@ -6802,3 +6802,46 @@ func findTheArrayConcVal(nums []int) int64 {
 	}
 	return int64(result)
 }
+
+// 896. 单调数列
+// 如果数组是单调递增或单调递减的，那么它是 单调 的。
+// 如果对于所有 i <= j，nums[i] <= nums[j]，那么数组 nums 是单调递增的。 如果对于所有 i <= j，nums[i]> = nums[j]，那么数组 nums 是单调递减的。
+// 当给定的数组 nums 是单调数组时返回 true，否则返回 false。
+//
+// 示例 1：
+// 输入：nums = [1,2,2,3]
+// 输出：true
+//
+// 示例 2：
+// 输入：nums = [6,5,4,4]
+// 输出：true
+//
+// 示例 3：
+// 输入：nums = [1,3,2]
+// 输出：false
+//
+// 提示：
+// 1 <= nums.length <= 105
+// -105 <= nums[i] <= 105
+func isMonotonic(nums []int) bool {
+	increase, decrease := true, true
+	n := len(nums)
+	if n <= 2 {
+		return true
+	}
+	for i := 1; i < n; i++ {
+		if nums[i-1] < nums[i] {
+			decrease = false
+			if !increase {
+				return false
+			}
+		}
+		if nums[i-1] > nums[i] {
+			increase = false
+			if !decrease {
+				return false
+			}
+		}
+	}
+	return increase || decrease
+}
