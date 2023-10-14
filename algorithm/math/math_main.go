ee1679f48ce343317f61cc0c1f33e11b6f278776
qingfengsa <15800012123@163.com>
2023-10-15 07:38:03 +0800

    LeetCode 2023-10-15 914. 卡牌分组

--- algorithm/math/math_main.go
+++ algorithm/math/math_main.go
@@ -4153,3 +4153,43 @@ func isRectangleOverlap(rec1 []int, rec2 []int) bool {
 	//
 	return max(x11, x21) < min(x12, x22) && max(y11, y21) < min(y12, y22)
 }
+
+// 914. 卡牌分组
+// 给定一副牌，每张牌上都写着一个整数。
+// 此时，你需要选定一个数字 X，使我们可以将整副牌按下述规则分成 1 组或更多组：
+//
+// 每组都有 X 张牌。
+// 组内所有的牌上都写着相同的整数。
+// 仅当你可选的 X >= 2 时返回 true。
+//
+// 示例 1：
+// 输入：deck = [1,2,3,4,4,3,2,1]
+// 输出：true
+// 解释：可行的分组是 [1,1]，[2,2]，[3,3]，[4,4]
+//
+// 示例 2：
+// 输入：deck = [1,1,1,2,2,2,3,3]
+// 输出：false
+// 解释：没有满足要求的分组。
+//
+// 提示：
+// 1 <= deck.length <= 104
+// 0 <= deck[i] < 104
+func hasGroupsSizeX(deck []int) bool {
+	countMap := make(map[int]int)
+	for _, num := range deck {
+		countMap[num]++
+	}
+	gcd := 0
+	for _, count := range countMap {
+		if count < 2 {
+			return false
+		}
+		if gcd == 0 {
+			gcd = count
+			continue
+		}
+		gcd = getGcd(gcd, count)
+	}
+	return gcd >= 2
+}
