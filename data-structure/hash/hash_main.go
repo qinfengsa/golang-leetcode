88aa6fba837a2c5f354c4f8bde1ac511020bc2cc
qingfengsa <15800012123@163.com>
2023-11-05 17:28:40 +0800

    LeetCode 2023-11-05 1002. 查找共用字符

--- data-structure/hash/hash_main.go
+++ data-structure/hash/hash_main.go
@@ -2141,3 +2141,43 @@ func countPoints(rings string) int {
 	}
 	return count
 }
+
+// 1002. 查找共用字符
+// 给你一个字符串数组 words ，请你找出所有在 words 的每个字符串中都出现的共用字符（ 包括重复字符），并以数组形式返回。你可以按 任意顺序 返回答案。
+//
+// 示例 1：
+// 输入：words = ["bella","label","roller"]
+// 输出：["e","l","l"]
+//
+// 示例 2：
+// 输入：words = ["cool","lock","cook"]
+// 输出：["c","o"]
+//
+// 提示：
+// 1 <= words.length <= 100
+// 1 <= words[i].length <= 100
+// words[i] 由小写英文字母组成
+func commonChars(words []string) []string {
+	minLetters := make([]int, 26)
+	for i := 0; i < 26; i++ {
+		minLetters[i] = math.MaxInt32
+	}
+	for _, word := range words {
+		letters := make([]int, 26)
+		for _, c := range word {
+			letters[c-'a']++
+		}
+		for i := 0; i < 26; i++ {
+			minLetters[i] = min(minLetters[i], letters[i])
+		}
+
+	}
+	result := make([]string, 0)
+	for i := 0; i < 26; i++ {
+		for minLetters[i] > 0 {
+			result = append(result, string(rune('a'+i)))
+			minLetters[i]--
+		}
+	}
+	return result
+}
